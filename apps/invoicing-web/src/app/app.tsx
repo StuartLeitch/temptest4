cb994185b722a985fddb705f1adfa765093d2b4f
Lucian Lature <lucian.lature@hindawi.com>
2019-10-24 11:13:22 +0300

    Split right side into multiple components.

--- apps/invoicing-web/src/app/app.tsx
+++ apps/invoicing-web/src/app/app.tsx
@@ -7,16 +7,12 @@ import JsonGraphqlServer from "json-graphql-server";
 import Row from "antd/es/row";
 import Col from "antd/es/col";
 import Card from "antd/es/card";
-import List from "antd/es/list";
-import Typography from "antd/es/typography";
-// import Avatar from "antd/es/avatar";
-// import Tabs from "antd/es/tabs";
-// import Icon from "antd/es/icon";
 
 // * components
-// import { Panel } from "./components/panel/panel";
 import { PaymentSteps } from "./components/payment-steps/payment-steps";
-// import CreditCardForm from "./components/credit-card-payment-form/credit-card-payment-form";
+import { ManuscriptDetails } from "./components/manuscript-details/manuscript-details";
+import { InvoiceDetails } from "./components/invoice-details/invoice-details";
+import { Charges } from "./components/charges/charges";
 
 import { appRedux, userRedux, manuscriptRedux, invoiceRedux } from "./state-management/redux";
 
@@ -28,40 +24,12 @@ const { fetchManuscriptAction } = manuscriptRedux;
 const { fetchInvoiceAction } = invoiceRedux;
 
 // * pages
-// import { Index as IndexContainer } from "./pages/index/index";
 import { IndexContainer } from "./pages/index/index-container";
 import { Payment } from "./pages/payment/payment";
 import { BillingAddress } from "./pages/billing-address/billing-address";
 
-const { Text } = Typography;
-// const { TabPane } = Tabs;
-
 // * app styles
 import "./app.scss";
-
-const articleDetails = [
-  ["Journal Title", "Advances in Condensed Matter Physics"],
-  ["Article Title", "RNA detection based on graphene field effect transistor biosensor"],
-  ["Article ID", "8146765"],
-  ["Article Type", "Research Article"],
-  ["CC License", "CC-BY 4.0"],
-  ["Corresponding Author", "Shicai Xu"],
-  ["Additional Authors", "view author list"],
-];
-
-const invoiceDetails = [
-  ["Invoice Issue Date", "19 September 2019"],
-  ["Date of Supply", "19 September 2019"],
-  ["Reference Number", "483/2019"],
-  ["Terms", "Payable upon Receipt"],
-];
-
-const charges = [
-  ["Article Processing Charges", 1250.0],
-  ["Net Charges", 1250.0],
-  ["VAT (+20%)", 250.0],
-  ["Total", 1500.0],
-];
 
 export const App = () => {
   const [current, setCurrent] = useState(0);
@@ -114,37 +82,9 @@ export const App = () => {
           </Col>
           <Col span={12}>
             <Card>
-              <Card type="inner" title="ARTICLE DETAILS">
-                <List
-                  dataSource={articleDetails}
-                  renderItem={item => (
-                    <List.Item>
-                      <Text strong>{item[0]}</Text> {item[1]}
-                    </List.Item>
-                  )}
-                />
-              </Card>
-              <Card type="inner" title="INVOICE DETAILS">
-                <List
-                  dataSource={invoiceDetails}
-                  renderItem={item => (
-                    <List.Item>
-                      <Text strong>{item[0]}</Text> {item[1]}
-                    </List.Item>
-                  )}
-                />
-              </Card>
-              <Card type="inner" title="CHARGES">
-                <List
-                  dataSource={charges}
-                  renderItem={item => (
-                    <List.Item>
-                      <Text strong>{item[0]}</Text> <Text strong>$</Text>
-                      {item[1]}
-                    </List.Item>
-                  )}
-                />
-              </Card>
+              <ManuscriptDetails title="ARTICLE DETAILS" />
+              <InvoiceDetails title="INVOICE DETAILS" />
+              <Charges title="CHARGES" />
             </Card>
           </Col>
         </Row>
