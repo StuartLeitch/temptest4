481ae70af3f45472e0beadca19f4e388a3ddc7bf
Lucian Lature <lucian.lature@hindawi.com>
2019-10-28 16:50:13 +0200

    Change affected.sh

--- scripts/affected.sh
+++ scripts/affected.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
-if [[ ! -f dist/last-deploy.txt ]] then
-  mkdir dist && git rev-parse HEAD~1 > dist/last-deploy.txt
+if [[ ! -f dist/last-deploy.txt ]]; then
+  mkdir dist && git rev-parse HEAD~1 > dist/last-deploy.txt;
 fi
+
 echo 'export AFFECTED_ARGS="--base $(cat dist/last-deploy.txt)"' >> $BASH_ENV
 source $BASH_ENV
 echo $AFFECTED_ARGS
