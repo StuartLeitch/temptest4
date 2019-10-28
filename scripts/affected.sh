d43dab73ae2d17593cfca747a0afbeffb9b586b3
Lucian Lature <lucian.lature@hindawi.com>
2019-10-28 17:52:23 +0200

    Export affected args.

--- scripts/affected.sh
+++ scripts/affected.sh
@@ -4,6 +4,4 @@ if [[ ! -f dist/last-deploy.txt ]]; then
   mkdir dist && git rev-parse HEAD~1 > dist/last-deploy.txt;
 fi
 
-echo 'export AFFECTED_ARGS="--base $(cat dist/last-deploy.txt)"' >> $AFFECTED
-source $AFFECTED
-echo $AFFECTED_ARGS
+echo '--base $(cat dist/last-deploy.txt)'
