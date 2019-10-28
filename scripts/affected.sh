36f4f6e223d24be7aaaa2ad2fdf918a57c0c5c65
Lucian Lature <lucian.lature@hindawi.com>
2019-10-28 17:39:02 +0200

    CI addon #1.

--- scripts/affected.sh
+++ scripts/affected.sh
@@ -4,6 +4,6 @@ if [[ ! -f dist/last-deploy.txt ]]; then
   mkdir dist && git rev-parse HEAD~1 > dist/last-deploy.txt;
 fi
 
-echo 'export AFFECTED_ARGS="--base $(cat dist/last-deploy.txt)"' >> $BASH_ENV
-source $BASH_ENV
+echo 'export AFFECTED_ARGS="--base $(cat dist/last-deploy.txt)"' >> $AFFECTED
+source $AFFECTED
 echo $AFFECTED_ARGS
