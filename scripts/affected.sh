38bac70e14045ea03fe7f1e414abe4330d1d81f7
Lucian Lature <lucian.lature@hindawi.com>
2019-10-28 19:29:58 +0200

    Ignore CLI client for now.

--- scripts/affected.sh
+++ scripts/affected.sh
@@ -4,4 +4,4 @@ if [[ ! -f dist/last-deploy.txt ]]; then
   mkdir dist && git rev-parse HEAD~1 > dist/last-deploy.txt;
 fi
 
-echo '--base $(cat dist/last-deploy.txt)'
+echo $(--base $(cat dist/last-deploy.txt))
