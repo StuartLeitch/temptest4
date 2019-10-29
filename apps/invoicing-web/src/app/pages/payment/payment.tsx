68196600ea66ce19e17a3d93e25094f209a08f01
malexsan1 <alexandru.munt@gmail.com>
2019-10-29 16:57:48 +0200

    add paypal button component

--- apps/invoicing-web/src/app/pages/payment/payment.tsx
+++ apps/invoicing-web/src/app/pages/payment/payment.tsx
@@ -9,6 +9,7 @@ import Tabs from "antd/es/tabs";
 import Icon from "antd/es/icon";
 
 import { Panel } from "../../components/panel/panel";
+import Paypal from "../../components/paypal/paypal";
 import CreditCardForm from "../../components/credit-card-payment-form/credit-card-payment-form";
 
 const { TabPane } = Tabs;
@@ -51,7 +52,19 @@ export const Payment = props => (
           }
           key="2"
         >
-          PayPal Payment Method
+          <Paypal
+            total={0.01}
+            currency="EUR"
+            onCancel={(...args) => {
+              console.log("sunt pe cancel", args);
+            }}
+            onError={(err) => {
+              console.log("a dat eroare", err);
+            }}
+            onSuccess={payment => {
+              console.log(payment);
+            }}
+          />
         </TabPane>
         <TabPane
           tab={
--- apps/invoicing-web/src/environments/environment.ts
+++ apps/invoicing-web/src/environments/environment.ts
@@ -4,4 +4,5 @@
 export const environment = {
   production: false,
   BT_TOKENIZATION_KEY: "sandbox_zjv5gc87_q867bfty9983kzbj",
+  PP_CLIENT_ID: "ATMfB4wjw_ZydEneN1lrSI_FfRCDY_G-gmILQoB63fYXvkfSpGVaG-JI0VLP6iPUUL97qwnbATRDUFel",
 };
