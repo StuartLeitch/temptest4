f802eece226600fa0d059b84fba49dd7ef3120e5
Lucian Lature <lucian.lature@hindawi.com>
2019-10-29 14:17:36 +0200

    Trigger deploy #8.

--- tools/scripts/dockerize.sh
+++ tools/scripts/dockerize.sh
@@ -1,10 +1,8 @@
-AFFECTED_APPS='invoicing-graphql'
-
 for APP in $AFFECTED_APPS
 do
   # BUILD="$(basename -- $dir)"
   echo "Build Docker image for application '${APP}' using dist files from 'dist/apps/${APP}'"
-  docker build -f ./Dockerfile -t $AWS_REGISTRY/$APP:$CI_COMMIT_SHA dist/apps/$APP
+  docker build -t $AWS_REGISTRY/$APP:$CI_COMMIT_SHA dist/apps/$APP
   echo "Pushed image to ${AWS_REGISTRY}/${APP}:${CI_COMMIT_SHA}"
   docker push $AWS_REGISTRY/$APP:$CI_COMMIT_SHA
   docker tag $AWS_REGISTRY/$APP:$CI_COMMIT_SHA $AWS_REGISTRY/$APP:dev
@@ -15,7 +13,7 @@ done
 for LIB in $AFFECTED_LIBS
 do
   echo "Build Docker image for library '${LIB}' using dist files from 'dist.libs/${LIB}'"
-  docker build -f ./Dockerfile -t $AWS_REGISTRY/$LIB:$CI_COMMIT_SHA dist/libs/$LIB
+  docker build -t $AWS_REGISTRY/$LIB:$CI_COMMIT_SHA dist/libs/$LIB
   echo "Pushed image to ${AWS_REGISTRY}/${LIB}:${CI_COMMIT_SHA}"
   docker push $AWS_REGISTRY/$LIB:$CI_COMMIT_SHA
   docker tag $AWS_REGISTRY/$LIB:$CI_COMMIT_SHA $AWS_REGISTRY/$LIB:dev
