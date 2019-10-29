8dcff229c6cb23d367d4215c9deeef561ae41f10
Lucian Lature <lucian.lature@hindawi.com>
2019-10-29 14:51:40 +0200

    Trigger deploy #11.

--- apps/invoicing-graphql/src/api/controllers/checkoutController.ts
+++ apps/invoicing-graphql/src/api/controllers/checkoutController.ts
@@ -20,7 +20,7 @@ export class CheckoutController {
 
   @Get()
   public hello(): Promise<any> {
-    return Promise.resolve({behehe: 'Hello World!'});
+    return Promise.resolve({muuuuu: 'Hello World!'});
   }
 
   @Post()
