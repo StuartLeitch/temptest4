ecd33d00b0f0265a652fa5b16f392fd6cddafc3f
Lucian Lature <lucian.lature@hindawi.com>
2019-10-31 10:26:27 +0200

    Trigger deploy #37.

--- apps/invoicing-web/src/app/components/paypal/paypal.tsx
+++ apps/invoicing-web/src/app/components/paypal/paypal.tsx
@@ -1,91 +1,91 @@
-import React, { useState, useEffect } from "react";
-import ReactDOM from "react-dom";
-import scriptLoader from "react-async-script-loader";
+// import React, { useState, useEffect } from "react";
+// import ReactDOM from "react-dom";
+// import scriptLoader from "react-async-script-loader";
 
-import { environment } from "../../../environments/environment";
+// import { environment } from "../../../environments/environment";
 
-interface Props {
-  total: number;
-  currency: string;
-  isScriptLoaded: boolean;
-  isScriptLoadSucceed: boolean;
-  onError?(data?: any): void;
-  onCancel?(data?: any): void;
-  onSuccess?(data?: any): void;
-}
+// interface Props {
+//   total: number;
+//   currency: string;
+//   isScriptLoaded: boolean;
+//   isScriptLoadSucceed: boolean;
+//   onError?(data?: any): void;
+//   onCancel?(data?: any): void;
+//   onSuccess?(data?: any): void;
+// }
 
-const CLIENT = {
-  sandbox: environment.PP_CLIENT_ID,
-  production: `${environment.PP_CLIENT_ID}_prod`,
-};
-const ENV = environment.production ? "production" : "sandbox";
+// const CLIENT = {
+//   sandbox: environment.PP_CLIENT_ID,
+//   production: `${environment.PP_CLIENT_ID}_prod`,
+// };
+// const ENV = environment.production ? "production" : "sandbox";
 
-const Paypal: React.FunctionComponent<Props> = ({
-  total,
-  currency,
-  onError,
-  onCancel,
-  onSuccess,
-  isScriptLoaded,
-  isScriptLoadSucceed,
-}) => {
-  const [showButton, setShowButton] = useState(false);
+// const Paypal: React.FunctionComponent<Props> = ({
+//   total,
+//   currency,
+//   onError,
+//   onCancel,
+//   onSuccess,
+//   isScriptLoaded,
+//   isScriptLoadSucceed,
+// }) => {
+//   const [showButton, setShowButton] = useState(false);
 
-  useEffect(() => {
-    window.React = React;
-    window.ReactDOM = ReactDOM;
-  }, []);
+//   useEffect(() => {
+//     window.React = React;
+//     window.ReactDOM = ReactDOM;
+//   }, []);
 
-  useEffect(() => {
-    if (!showButton && isScriptLoadSucceed && isScriptLoaded) {
-      setShowButton(true);
-    }
-  }, [showButton, isScriptLoadSucceed, isScriptLoaded]);
+//   useEffect(() => {
+//     if (!showButton && isScriptLoadSucceed && isScriptLoaded) {
+//       setShowButton(true);
+//     }
+//   }, [showButton, isScriptLoadSucceed, isScriptLoaded]);
 
-  const payment = () =>
-    paypal.rest.payment.create(ENV, CLIENT, {
-      transactions: [
-        {
-          amount: {
-            total,
-            currency,
-          },
-        },
-      ],
-    });
+//   const payment = () =>
+//     paypal.rest.payment.create(ENV, CLIENT, {
+//       transactions: [
+//         {
+//           amount: {
+//             total,
+//             currency,
+//           },
+//         },
+//       ],
+//     });
 
-  const onAuthorize = (data, actions) =>
-    actions.payment.execute().then(() => {
-      const payment = {
-        paid: true,
-        cancelled: false,
-        payerID: data.payerID,
-        paymentID: data.paymentID,
-        paymentToken: data.paymentToken,
-        returnUrl: data.returnUrl,
-      };
-      onSuccess(payment);
-    }, onError);
+//   const onAuthorize = (data, actions) =>
+//     actions.payment.execute().then(() => {
+//       const payment = {
+//         paid: true,
+//         cancelled: false,
+//         payerID: data.payerID,
+//         paymentID: data.paymentID,
+//         paymentToken: data.paymentToken,
+//         returnUrl: data.returnUrl,
+//       };
+//       onSuccess(payment);
+//     }, onError);
 
-  return (
-    showButton && (
-      <paypal.Button.react
-        env={ENV}
-        commit={true}
-        client={CLIENT}
-        payment={payment}
-        currency={currency}
-        onError={onError}
-        onCancel={onCancel}
-        onAuthorize={onAuthorize}
-      />
-    )
-  );
-};
+//   return (
+//     showButton && (
+//       <paypal.Button.react
+//         env={ENV}
+//         commit={true}
+//         client={CLIENT}
+//         payment={payment}
+//         currency={currency}
+//         onError={onError}
+//         onCancel={onCancel}
+//         onAuthorize={onAuthorize}
+//       />
+//     )
+//   );
+// };
 
-Paypal.defaultProps = {
-  currency: "USD",
-  total: 0.01,
-};
+// Paypal.defaultProps = {
+//   currency: "USD",
+//   total: 0.01,
+// };
 
-export default scriptLoader("https://www.paypalobjects.com/api/checkout.js")(Paypal);
+// export default scriptLoader("https://www.paypalobjects.com/api/checkout.js")(Paypal);
