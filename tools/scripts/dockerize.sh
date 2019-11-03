0cfc560cca4de03e7feac6aaf36c5b044f3e5c5f
Cezar Berea <cezar.berea@hindawi.com>
2019-11-03 18:36:21 +0200

    Only try to build images for libs or apps that have Dockerfiles

--- tools/scripts/dockerize.sh
+++ tools/scripts/dockerize.sh
@@ -1,25 +1,28 @@
 # AFFECTED_APPS='invoicing-web'
 for APP in $AFFECTED_APPS
 do
-  echo "Copy .env into application '${APP}'"
-  cp .env dist/apps/$APP/.env
-  echo "Build Docker image for application '${APP}' using dist files from 'dist/apps/${APP}'"
-  # docker build -f dist/apps/$APP/Dockerfile -t dutch .
-  docker build -f dist/apps/$APP/Dockerfile -t $AWS_REGISTRY/$APP:$CI_COMMIT_SHA .
-  echo "Pushed image to ${AWS_REGISTRY}/${APP}:${CI_COMMIT_SHA}"
-  docker push $AWS_REGISTRY/$APP:$CI_COMMIT_SHA
-  docker tag $AWS_REGISTRY/$APP:$CI_COMMIT_SHA $AWS_REGISTRY/$APP:dev
-  echo "Pushed image to ${AWS_REGISTRY}/${APP}:dev"
-  docker push $AWS_REGISTRY/$APP:dev
+  if [ -s "dist/apps/${APP}/Dockerfile" ]
+  then
+    echo "Building dist/apps/${APP}/Dockerfile"
+    docker build -f dist/apps/$APP/Dockerfile -t $AWS_REGISTRY/$APP:$CI_COMMIT_SHA .
+    docker push $AWS_REGISTRY/$APP:$CI_COMMIT_SHA
+    docker tag $AWS_REGISTRY/$APP:$CI_COMMIT_SHA $AWS_REGISTRY/$APP:dev
+    docker push $AWS_REGISTRY/$APP:dev
+  else
+    echo "Application ${APP} doesn't build a docker image. SKIPPING."
+  fi
 done
 
 for LIB in $AFFECTED_LIBS
 do
-  echo "Build Docker image for library '${LIB}' using dist files from 'dist.libs/${LIB}'"
-  docker build -t $AWS_REGISTRY/$LIB:$CI_COMMIT_SHA dist/libs/$LIB
-  echo "Pushed image to ${AWS_REGISTRY}/${LIB}:${CI_COMMIT_SHA}"
-  docker push $AWS_REGISTRY/$LIB:$CI_COMMIT_SHA
-  docker tag $AWS_REGISTRY/$LIB:$CI_COMMIT_SHA $AWS_REGISTRY/$LIB:dev
-  echo "Pushed image to ${AWS_REGISTRY}/${LIB}:dev"
-  docker push $AWS_REGISTRY/$LIB:dev
+  if [ -s "dist/libs/${LIB}/Dockerfile" ]
+  then
+    echo "Building dist/libs/${LIB}/Dockerfile"
+    docker build -t $AWS_REGISTRY/$LIB:$CI_COMMIT_SHA dist/libs/$LIB
+    docker push $AWS_REGISTRY/$LIB:$CI_COMMIT_SHA
+    docker tag $AWS_REGISTRY/$LIB:$CI_COMMIT_SHA $AWS_REGISTRY/$LIB:dev
+    docker push $AWS_REGISTRY/$LIB:dev
+  else
+    echo "Library ${LIB} doesn't build a docker image. SKIPPING."
+  fi
 done
