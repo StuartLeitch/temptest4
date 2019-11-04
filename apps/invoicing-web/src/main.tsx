006c8ff3d1ede2b00b3a0372cb51029a44f66a78
malexsan1 <alexandru.munt@gmail.com>
2019-11-04 13:29:35 +0200

    update env sample

--- apps/invoicing-web/src/main.tsx
+++ apps/invoicing-web/src/main.tsx
@@ -2,17 +2,9 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
 import { BrowserRouter } from "react-router-dom";
-import JsonGraphqlServer from "json-graphql-server";
 
 import { configureStore } from "./app/frameworks/redux";
 import App from "./app/app";
-import data from "./app/db";
-
-const server = JsonGraphqlServer({
-  data,
-  url: "http://localhost:4200/graphql",
-});
-server.start();
 
 const store: any = configureStore();
 
