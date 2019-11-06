043e22033da619edd2062f004b87c1d9f451b565
malexsan1 <alexandru.munt@gmail.com>
2019-11-06 18:03:59 +0200

    remove config

--- apps/invoicing-web/src/main.tsx
+++ apps/invoicing-web/src/main.tsx
@@ -6,26 +6,10 @@ import { ThemeProvider } from "styled-components";
 import { theme } from "@hindawi/react-components";
 
 import App from "./app/app";
-import { AuthService } from './app/services/authService';
-import { config } from './config';
-import axios from 'axios';
-
-const authService = new AuthService(config);
-authService.init();
-authService.$state.subscribe((s) => {
-  if (s.session) {
-    axios.get(`${config.apiRoot}/jwt-test`, {
-      params: {
-        token: s.session.token
-      }
-    }).then(r => console.log(r.data));
-  }
-});
-
-import { config } from './config';
-import { Context } from './context';
+import { config } from "./config";
+import { Context } from "./context";
 import { makeStore } from "./app/state";
-import { actions } from './app/state/modules/system';
+import { actions } from "./app/state/modules/system";
 
 const context = new Context(config);
 const store = makeStore(config, context);
@@ -40,6 +24,6 @@ ReactDOM.render(
   </Provider>,
   document.getElementById("root"),
   () => {
-    store.dispatch(actions.init())
-  }
+    store.dispatch(actions.init());
+  },
 );
