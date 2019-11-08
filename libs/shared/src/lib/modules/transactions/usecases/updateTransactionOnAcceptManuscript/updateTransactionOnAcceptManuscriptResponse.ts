ae6f79b77324934d6900694045b26b51b55d9056
Lucian Lature <lucian.lature@hindawi.com>
2019-11-08 10:55:52 +0200

    Trigger deploy.

--- libs/shared/src/lib/modules/transactions/usecases/updateTransactionOnAcceptManuscript/updateTransactionOnAcceptManuscriptResponse.ts
+++ libs/shared/src/lib/modules/transactions/usecases/updateTransactionOnAcceptManuscript/updateTransactionOnAcceptManuscriptResponse.ts
@@ -1,13 +1,10 @@
 import {Either, Result} from '../../../../core/logic/Result';
 import {AppError} from '../../../.././core/logic/AppError';
 
-// import {UpvoteCommentErrors} from './UpvoteCommentErrors';
-// import {UpvotePostErrors} from '../../post/upvotePost/UpvotePostErrors';
+// import {UpdateTransactionErrors} from './updateTransactionErrors';
 
 export type UpdateTransactionOnAcceptManuscriptResponse = Either<
-  // | UpvotePostErrors.PostNotFoundError
-  // | UpvoteCommentErrors.CommentNotFoundError
-  // | UpvoteCommentErrors.MemberNotFoundError
-  AppError.UnexpectedError | Result<any>,
+  // | UpdateTransactionErrors.SomethingWrong
+  AppError.UnexpectedError,
   Result<void>
 >;
