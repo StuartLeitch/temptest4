bc14e7092a6a8a8fb9f6905acbd87fba803cbb48
Lucian Lature <lucian.lature@hindawi.com>
2019-11-08 09:47:16 +0200

    Run tests for all affected apps.

--- tools/scripts/affected_apps.sh
+++ tools/scripts/affected_apps.sh
@@ -1,3 +1,3 @@
 # affected_apps=$(npm run affected:apps -- ${AFFECTED_APPS} --plain 2>&1 | tail -1)
 
-echo $(npm run affected:apps -- --plain 2>&1 | tail -1 | tr " " "\n");
+echo $(npm run affected:apps -- -- ${AFFECTED_APPS} --plain 2>&1 | tail -1 | tr " " "\n");
