04cf6fbf77908db8acda68cdd8581f6a6c98fd93
Cezar Berea <cezar.berea@hindawi.com>
2019-11-08 13:33:42 +0200

    Fix deploy script

--- tools/scripts/deploy.sh
+++ tools/scripts/deploy.sh
@@ -1,14 +1,2 @@
-#  aws elasticbeanstalk create-application-version \
-#       --application-name Invoicing-graphql-dev --version-label invoicing-graphql-dev
-# AFFECTED_APPS='invoicing-web'
-for APP in $AFFECTED_APPS
-do
-  echo "Deploy application on environment '${APP}-dev'"
-  aws elasticbeanstalk update-environment --environment-name $APP-dev --version-label $APP-dev
-done
-
-for LIB in $AFFECTED_LIBS
-do
-  echo "Deploy library on environment '${LIB}-dev'"
-  aws elasticbeanstalk update-environment --environment-name $LIB-dev --version-label $LIB-dev
-done
+aws elasticbeanstalk update-environment --environment-name invoicing-graphql-dev --version-label invoicing-graphql-dev
+aws elasticbeanstalk update-environment --environment-name invoicing-web-dev --version-label invoicing-web-dev
