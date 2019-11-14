f3b41780686de008ddf31c68180828cae0417116
Lucian Lature <lucian.lature@hindawi.com>
2019-11-14 12:23:18 +0200

    Arghhh

--- tools/scripts/deploy.sh
+++ tools/scripts/deploy.sh
@@ -1,2 +1,2 @@
-aws elasticbeanstalk update-environment --environment-name dev-invoicing-graphql --version-label dev-invoicing-graphql
+aws elasticbeanstalk update-environment --environment-name dev-invoicing-graphql --version-label invoicing-graphql-dev
 aws elasticbeanstalk update-environment --environment-name invoicing-web-dev --version-label invoicing-web-dev
