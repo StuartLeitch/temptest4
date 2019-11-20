dc7fe7b2aaeddb9d1d629c23b43ba74039ad0352
malexsan1 <alexandru.munt@gmail.com>
2019-11-20 11:29:02 +0200

    remove seeds

--- libs/shared/src/lib/infrastructure/database/knex/seeds/articles.js
+++ libs/shared/src/lib/infrastructure/database/knex/seeds/articles.js
@@ -2,22 +2,20 @@
 const { v4 } = require('uuid');
 
 const seed = async knex => {
-  console.log('seeding articles...');
-  await knex('articles').truncate();
-
-  const article = {
-    id: '07e030f2-08d4-45c2-be22-3c2ac7a32057',
-    journalId: '0dc559c7-ed6d-48f0-a7c9-3dad40f3144e',
-    title: 'Mos Craciun si prietenii sai',
-    articleType: 'Research Article',
-    authorEmail: 'santa.claus@megamail.xyz',
-    authorCountry: 'FI',
-    authorFirstName: 'Klaus Werner',
-    authorSurname: 'Iohannis',
-    customId: '1234567'
-  };
-
-  await knex('articles').insert(article);
+  // console.log('seeding articles...');
+  // await knex('articles').truncate();
+  // const article = {
+  //   id: '07e030f2-08d4-45c2-be22-3c2ac7a32057',
+  //   journalId: '0dc559c7-ed6d-48f0-a7c9-3dad40f3144e',
+  //   title: 'Mos Craciun si prietenii sai',
+  //   articleType: 'Research Article',
+  //   authorEmail: 'santa.claus@megamail.xyz',
+  //   authorCountry: 'FI',
+  //   authorFirstName: 'Klaus Werner',
+  //   authorSurname: 'Iohannis',
+  //   customId: '1234567'
+  // };
+  // await knex('articles').insert(article);
 };
 
 exports.seed = seed;
--- libs/shared/src/lib/infrastructure/database/knex/seeds/journals.js
+++ libs/shared/src/lib/infrastructure/database/knex/seeds/journals.js
@@ -2,20 +2,18 @@
 const { v4 } = require('uuid');
 
 const seed = async knex => {
-  console.log('seeding journals...');
-  await knex('journals').truncate();
-
-  const journal = {
-    id: '0dc559c7-ed6d-48f0-a7c9-3dad40f3144e',
-    name: 'Stiinta si Viata',
-    apc: 1000,
-    code: 'SV',
-    email: 'stiintasiviata@jurnal.zyx',
-    issn: '123-123123-123',
-    isActive: 1
-  };
-
-  await knex('journals').insert(journal);
+  // console.log('seeding journals...');
+  // await knex('journals').truncate();
+  // const journal = {
+  //   id: '0dc559c7-ed6d-48f0-a7c9-3dad40f3144e',
+  //   name: 'Stiinta si Viata',
+  //   apc: 1000,
+  //   code: 'SV',
+  //   email: 'stiintasiviata@jurnal.zyx',
+  //   issn: '123-123123-123',
+  //   isActive: 1
+  // };
+  // await knex('journals').insert(journal);
 };
 
 exports.seed = seed;
