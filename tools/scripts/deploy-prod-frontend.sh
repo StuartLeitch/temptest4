3d6ef37ca8d0f7e95f6fe049891754ba7abf0b63
Lucian Lature <lucian.lature@hindawi.com>
2019-12-02 16:56:01 +0200

    Fixed nginx configuration for the web apps.

--- tools/scripts/deploy-prod-frontend.sh
+++ tools/scripts/deploy-prod-frontend.sh
@@ -10,4 +10,4 @@ printf -- "Dockerize FrontEnd App for ${AWS_ENVIRONMENT} environment"
 APP=$APP TO=$TO AWS_ENVIRONMENT=$AWS_ENVIRONMENT AWS_REGISTRY=$AWS_REGISTRY CI_COMMIT_SHA=$CI_COMMIT_SHA ./tools/scripts/dockerize-local.sh
 
 printf -- "Deploy FrontEnd App for ${AWS_ENVIRONMENT} environment"
-aws --profile=production elasticbeanstalk update-environment --environment-name demo-invoicing-web --version-label demo-invoicing-web
+aws --profile=production elasticbeanstalk update-environment --environment-name $AWS_ENVIRONMENT-invoicing-web --version-label $AWS_ENVIRONMENT-invoicing-web
--- tools/scripts/deploy-prod.sh
+++ tools/scripts/deploy-prod.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-export AWS_REGISTRY="496598730381.dkr.ecr.eu-west-1.amazonaws.com"
-export AWS_ENVIRONMENT="demo"
+export AWS_REGISTRY="918602980697.dkr.ecr.eu-west-1.amazonaws.com"
+export AWS_ENVIRONMENT="prod"
 export APP="invoicing-graphql"
 export TO="${AWS_ENVIRONMENT}-${APP}"
 export TIMESTAMP=$(date +'%d_%m_%Y_%R')
@@ -25,10 +25,10 @@ fi;
 
 CURR_DIR="$(dirname $0)"
 
-printf -- '\033[33m>\033[0m Deploy Back End App for "demo" environment\n'
+printf -- '\033[33m\033[1m > Deploy Back End App for "prod" environment\033[0m\n'
 ./$CURR_DIR/deploy-$AWS_ENVIRONMENT-backend.sh
 
-printf -- '\033[33m>\033[0m Deploy Front End App for "demo" environment\n'
+printf -- '\033[33m\033[1m > Deploy Front End App for "prod" environment\033[0m\n'
 ./$CURR_DIR/deploy-$AWS_ENVIRONMENT-frontend.sh
 
 printf -- '\n';
