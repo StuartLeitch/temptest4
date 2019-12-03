8860f9be5f85d2d322844c965b2b352ff79c3667
Raul Popovici <raulpopovici@Rauls-MacBook-Pro-2.local>
2019-12-03 15:13:03 +0200

    fix scripts

--- tools/scripts/deploy-demo-backend.sh
+++ tools/scripts/deploy-demo-backend.sh
@@ -1,7 +1,5 @@
 #!/bin/sh
 
-# AWS_SNS_SQS_ACCESS_KEY=AKIAXHH4XI2G67H4L2RE AWS_SNS_SQS_SECRET_KEY=U0AsyIfhG2JLSLLvbH6IUzsWX9v4h8HmXZzoC7o DB_PASSWORD=Vnc2etyLoFaeg39 AWS_SES_ACCESS_KEY=AKIAIGOL6SNQYTP2HWNA AWS_SES_SECRET_KEY=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 AWS_SES_REGION=eu-west-1 BT_MERCHANT_ID=q867bfty9983kzbj BT_PRIVATE_KEY=525102f862711c6614a0274b4913c96e BT_PUBLIC_KEY=dng24zqrqxbvbrh8 BT_ENVIRONMENT=sandbox DB_DATABASE=demo_invoicing DB_HOST=demo-invoicing-graphql.crznwonwxglm.eu-west-1.rds.amazonaws.com DB_MIGRATIONS_DIR=/migrations DB_PASSWORD=Vnc2etyLoFaeg39 DB_USERNAME=demo_invoicing_user npm start invoicing-graphql
-
 printf -- "\nBuild BackEnd App for ${AWS_ENVIRONMENT} environment\n"
 npm run build invoicing-graphql --configuration=production
 
