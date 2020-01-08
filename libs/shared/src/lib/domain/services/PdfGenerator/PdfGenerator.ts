9df2c4031592fe48422b3cb97abe770e2128474f
Raul Popovici <raulpopovici@Rauls-MacBook-Pro-2.local>
2020-01-08 12:10:29 +0200

    fix docker image

--- libs/shared/src/lib/domain/services/PdfGenerator/PdfGenerator.ts
+++ libs/shared/src/lib/domain/services/PdfGenerator/PdfGenerator.ts
@@ -42,8 +42,7 @@ export class PdfGeneratorService {
   }
 
   public async getInvoice(payload: InvoicePayload): Promise<Readable> {
-    const logoUrl =
-      'http://demo-gsw-invoicing-web.eu-west-1.elasticbeanstalk.com/assets/gsw/images/Lithosphere_logo_web.png';
+    const logoUrl = process.env.LOGO_URL;
     const logoData = await PdfGeneratorService.convertLogo(logoUrl);
 
     return new Promise((resolve, reject) => {
