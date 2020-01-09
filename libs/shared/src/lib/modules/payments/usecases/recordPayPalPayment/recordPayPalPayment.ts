6b29b8bcf8e2415f9eefc1be60a27d0b161aaa0b
Raul Popovici <raulpopovici@Rauls-MacBook-Pro-2.local>
2020-01-09 16:21:04 +0200

    fix record paypal usecase

--- libs/shared/src/lib/modules/payments/usecases/recordPayPalPayment/recordPayPalPayment.ts
+++ libs/shared/src/lib/modules/payments/usecases/recordPayPalPayment/recordPayPalPayment.ts
@@ -101,7 +101,7 @@ export class RecordPayPalPaymentUsecase
   private async getPayPalOrderDetails(orderId: string) {
     try {
       const request = new checkoutNodeJsSDK.orders.OrdersGetRequest(orderId);
-      const order = await this.payPalService().execute(request);
+      const order = await this.payPalService.execute(request);
       console.log(`PayPal Order ID: ${orderId}`);
       return right(order.result);
     } catch (e) {
