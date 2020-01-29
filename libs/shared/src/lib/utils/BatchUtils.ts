dc964db5ff1afbee45648e2a1032642ef2f17c7f
Rares Stan <rares.stan@hindawi.com>
2020-01-29 11:08:33 +0200

    Add batching with timeout for generators

--- libs/shared/src/lib/utils/BatchUtils.ts
+++ libs/shared/src/lib/utils/BatchUtils.ts
@@ -27,4 +27,37 @@ export class BatchUtils {
       }
     };
   }
+
+  public static generatorWithTimeout<T, U = void>(
+    callBack: (batch: T[]) => U,
+    batchSize: number,
+    timeout: number
+  ): (generator: AsyncGenerator<T, any, unknown>) => Promise<void> {
+    let timeoutHandler: NodeJS.Timeout;
+    let batch: T[] = [];
+
+    const clearBatch = () => {
+      while (batch.length) {
+        callBack(batch.splice(0, batchSize));
+      }
+    };
+
+    return async generator => {
+      timeoutHandler = setTimeout(clearBatch, timeout);
+
+      for await (const obj of generator) {
+        batch.push(obj);
+
+        if (batch.length >= batchSize) {
+          clearTimeout(timeoutHandler);
+          clearBatch();
+          timeoutHandler = setTimeout(clearBatch, timeout);
+        }
+      }
+
+      if (timeoutHandler) {
+        clearTimeout(timeoutHandler);
+      }
+    };
+  }
 }
