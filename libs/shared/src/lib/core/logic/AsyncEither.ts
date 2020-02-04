10e8f32f7b2d5decd604e6f96ded6d47bab8392f
Rares Stan <rares.stan@hindawi.com>
2020-02-04 17:47:14 +0200

    Change chain to asyncChain and add normal chain method to the AsyncEither Class

--- libs/shared/src/lib/core/logic/AsyncEither.ts
+++ libs/shared/src/lib/core/logic/AsyncEither.ts
@@ -1,9 +1,18 @@
 import { Either } from './Result';
 import { Right } from './Right';
 
+type MutationType = 'map' | 'chain' | 'asyncChain';
+type MutationFunction = (r: any) => any;
+
+type ObjectMapping<L, R> = {
+  [key in MutationType | 'default']: (
+    val: Either<L, R>
+  ) => (fn: MutationFunction) => Promise<Either<L, R>> | Either<L, R>;
+};
+
 interface Mutation {
-  type: 'map' | 'chain';
-  fn: (r: any) => any;
+  fn: MutationFunction;
+  type: MutationType;
 }
 
 export class AsyncEither<L, R> {
@@ -20,7 +29,12 @@ export class AsyncEither<L, R> {
     return (this as unknown) as AsyncEither<L, R2>;
   }
 
-  chain<L2, R2>(fn: (r: R) => Promise<Either<L2, R2>>) {
+  asyncChain<L2, R2>(fn: (r: R) => Promise<Either<L2, R2>>) {
+    this.mutations.push({ type: 'asyncChain', fn });
+    return (this as unknown) as AsyncEither<L2 | L, R2>;
+  }
+
+  chain<L2, R2>(fn: (r: R) => Either<L2, R2>) {
     this.mutations.push({ type: 'chain', fn });
     return (this as unknown) as AsyncEither<L2 | L, R2>;
   }
@@ -28,15 +42,21 @@ export class AsyncEither<L, R> {
   async execute(): Promise<Either<L, R>> {
     let val: Either<L, R> = new Right(this.value);
     for (const mutation of this.mutations) {
-      if (mutation.type === 'map') {
-        val = val.map(mutation.fn);
-      } else {
-        val = await ((val.chain(mutation.fn) as unknown) as Promise<
-          Either<any, any>
-        >);
-      }
+      val = await this.functionToApply<L, R>(mutation.type, val)(mutation.fn);
     }
 
     return val;
+  }
+
+  private functionToApply<L, R>(type: MutationType, val: Either<L, R>) {
+    const objectMapping: ObjectMapping<L, R> = {
+      asyncChain: (val: Either<L, R>) => (fn: MutationFunction) =>
+        (val.chain(fn) as unknown) as Promise<Either<L, R>>,
+      chain: (val: Either<L, R>) => (fn: MutationFunction) =>
+        val.chain<L, R>(fn),
+      map: (val: Either<L, R>) => (fn: MutationFunction) => val.map<R>(fn),
+      default: (val: Either<L, R>) => () => val
+    };
+    return (objectMapping[type] || objectMapping.default)(val);
   }
 }
