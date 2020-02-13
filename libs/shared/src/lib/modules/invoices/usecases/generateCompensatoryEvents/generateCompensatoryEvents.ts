830dfb4c44e205ad7e6fce00cf38b4cc860a0bdc
Rares Stan <rares.stan@hindawi.com>
2020-02-13 10:52:21 +0200

    Add the publish invoice created logic

--- libs/shared/src/lib/modules/invoices/usecases/generateCompensatoryEvents/generateCompensatoryEvents.ts
+++ libs/shared/src/lib/modules/invoices/usecases/generateCompensatoryEvents/generateCompensatoryEvents.ts
@@ -6,24 +6,40 @@ import { UniqueEntityID } from '../../../../core/domain/UniqueEntityID';
 import { AsyncEither } from '../../../../core/logic/AsyncEither';
 
 // * Authorization Logic
-import {
-  Authorize,
-  AuthorizationContext,
-  AccessControlledUsecase
-} from '../../../../domain/authorization/decorators/Authorize';
 import { AccessControlContext } from '../../../../domain/authorization/AccessControl';
 import { Roles } from '../../../users/domain/enums/Roles';
+import {
+  AccessControlledUsecase,
+  AuthorizationContext,
+  Authorize
+} from '../../../../domain/authorization/decorators/Authorize';
+
+import { SQSPublishServiceContract } from '../../../../domain/services/SQSPublishService';
 
 import { InvoiceId } from '../../domain/InvoiceId';
-import { Invoice } from '../../domain/Invoice';
+import { Invoice, InvoiceStatus } from '../../domain/Invoice';
 
+import { ArticleRepoContract } from '../../../manuscripts/repos/articleRepo';
+import { CouponRepoContract } from '../../../coupons/repos/couponRepo';
+import { WaiverRepoContract } from '../../../waivers/repos/waiverRepo';
+import { InvoiceItemRepoContract } from '../../repos/invoiceItemRepo';
 import { InvoiceRepoContract } from '../../repos/invoiceRepo';
+
+import { GetManuscriptByInvoiceIdUsecase } from '../../../manuscripts/usecases/getManuscriptByInvoiceId';
+import { GetItemsForInvoiceUsecase } from '../getItemsForInvoice/getItemsForInvoice';
+import { GetInvoiceDetailsUsecase } from '../getInvoiceDetails/getInvoiceDetails';
+
+import { PublishInvoiceConfirmed } from '../publishInvoiceConfirmed';
+import { PublishInvoicePaid } from '../publishInvoicePaid';
+import {
+  PublishInvoiceCreatedUsecase,
+  PublishInvoiceCreatedDTO
+} from '../publishInvoiceCreated';
 
 // * Usecase specific
 import { GenerateCompensatoryEventsResponse as Response } from './generateCompensatoryEventsResponse';
 import { GenerateCompensatoryEventsErrors as Errors } from './generateCompensatoryEventsErrors';
 import { GenerateCompensatoryEventsDTO as DTO } from './generateCompensatoryEventsDTO';
-import { GetInvoiceDetailsUsecase } from '../getInvoiceDetails/getInvoiceDetails';
 
 type Context = AuthorizationContext<Roles>;
 export type GenerateCompensatoryEventsContext = Context;
@@ -36,7 +52,14 @@ export class GenerateCompensatoryEventsUsecase
       GenerateCompensatoryEventsContext,
       AccessControlContext
     > {
-  constructor(private invoiceRepo: InvoiceRepoContract) {}
+  constructor(
+    private invoiceItemRepo: InvoiceItemRepoContract,
+    private sqsPublish: SQSPublishServiceContract,
+    private manuscriptRepo: ArticleRepoContract,
+    private invoiceRepo: InvoiceRepoContract,
+    private couponRepo: CouponRepoContract,
+    private waiverRepo: WaiverRepoContract
+  ) {}
 
   private async getAccessControlContext(request, context?) {
     return {};
@@ -47,9 +70,9 @@ export class GenerateCompensatoryEventsUsecase
     request: DTO,
     context?: GenerateCompensatoryEventsContext
   ): Promise<Response> {
-    const requestExecution = new AsyncEither<null, DTO>(request).then(
-      this.verifyInput
-    );
+    const requestExecution = new AsyncEither<null, DTO>(request)
+      .then(this.verifyInput)
+      .then(request => this.publishInvoiceCreated(request));
 
     return null;
   }
@@ -68,5 +91,73 @@ export class GenerateCompensatoryEventsUsecase
     const usecase = new GetInvoiceDetailsUsecase(this.invoiceRepo);
     const maybeResult = await usecase.execute({ invoiceId });
     return maybeResult.map(result => result.getValue());
+  }
+
+  private async getInvoiceItemsForInvoiceId(invoiceId: InvoiceId) {
+    const usecase = new GetItemsForInvoiceUsecase(
+      this.invoiceItemRepo,
+      this.couponRepo,
+      this.waiverRepo
+    );
+    const maybeResult = await usecase.execute({
+      invoiceId: invoiceId.id.toString()
+    });
+    return maybeResult.map(result => result.getValue());
+  }
+
+  private async getManuscriptByInvoiceId(invoiceId: InvoiceId) {
+    const usecase = new GetManuscriptByInvoiceIdUsecase(
+      this.manuscriptRepo,
+      this.invoiceItemRepo
+    );
+    const maybeResult = await usecase.execute({
+      invoiceId: invoiceId.id.toString()
+    });
+    return maybeResult.map(result => result.getValue());
+  }
+
+  private async publishInvoiceCreated(request: DTO) {
+    const publishUsecase = new PublishInvoiceCreatedUsecase(this.sqsPublish);
+    const execution = new AsyncEither<null, null>(null)
+      .then(() => this.getInvoiceWithId(request.invoiceId))
+      .then(async invoice => {
+        const maybeItems = await this.getInvoiceItemsForInvoiceId(
+          invoice.invoiceId
+        );
+        return maybeItems.map(invoiceItems => ({ invoice, invoiceItems }));
+      })
+      .then(async ({ invoice, invoiceItems }) => {
+        const maybeManuscript = await this.getManuscriptByInvoiceId(
+          invoice.invoiceId
+        );
+        return maybeManuscript.map(manuscripts => ({
+          manuscript: manuscripts[0],
+          invoice,
+          invoiceItems
+        }));
+      })
+      .map(data => {
+        const { invoice } = data;
+        if (!invoice.dateAccepted || invoice.status === InvoiceStatus.DRAFT) {
+          return null;
+        }
+        invoice.props.dateUpdated = invoice.dateAccepted;
+        invoice.status = InvoiceStatus.DRAFT;
+
+        return {
+          ...data,
+          invoice,
+          messageTimestamp: invoice.dateAccepted
+        };
+      })
+      .then(async publishRequest => {
+        if (!publishRequest) {
+          return right<null, null>(null);
+        }
+
+        return publishUsecase.execute(publishRequest);
+      })
+      .map(() => request);
+    return execution.execute();
   }
 }
