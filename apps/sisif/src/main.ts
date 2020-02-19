8d2f929696c13c9f81ca835150e531e7056bfbcb
Raul Popovici <raulpopovici@192-168-0-102.rdsnet.ro>
2020-02-19 23:01:57 +0200

    sisif secrets + agendash

--- apps/sisif/src/main.ts
+++ apps/sisif/src/main.ts
@@ -4,8 +4,17 @@ var Agenda = require('agenda');
 var Agendash = require('agendash');
 
 const connectionString = `mongodb://root:${process.env.MONGO_PASSWORD}@sisif-persistance-mongodb:${process.env.SISIF_PERSISTANCE_MONGODB_SERVICE_PORT_MONGODB}/admin`;
-
+console.log(connectionString);
 var agenda = new Agenda({ db: { address: connectionString } });
+agenda.define('log', async job => {
+  console.log(job.attrs.data);
+});
+(async function() {
+  // IIFE to give access to async/await
+  await agenda.start();
+
+  // agenda.every('10 seconds', 'log', { to: 'admin@example.com' });
+})();
 // or provide your own mongo client:
 // var agenda = new Agenda({mongo: myMongoClient})
 
