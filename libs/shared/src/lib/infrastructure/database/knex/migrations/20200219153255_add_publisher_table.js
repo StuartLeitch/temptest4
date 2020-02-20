3123c94db16a23818fb26092c706c542775c75d2
Rares Stan <rares.stan@hindawi.com>
2020-02-20 11:00:11 +0200

    Add default values for creation

--- libs/shared/src/lib/infrastructure/database/knex/migrations/20200219153255_add_publisher_table.js
+++ libs/shared/src/lib/infrastructure/database/knex/migrations/20200219153255_add_publisher_table.js
@@ -2,6 +2,7 @@ module.exports.up = async function(knex) {
   await knex.schema.createTable('publishers', table => {
     table.string('id', 40).primary();
     table.string('name');
+    table.unique('name');
   });
 
   await knex.schema.createTable('publisher_custom_values', table => {
@@ -12,10 +13,77 @@ module.exports.up = async function(knex) {
     table.primary(['name', 'publisherId']);
   });
 
+  await knex('publishers').insert([
+    {
+      id: '1',
+      name: 'Hindawi'
+    },
+    {
+      id: '2',
+      name: 'Wiley'
+    }
+  ]);
+
   await knex.schema.table('catalog', table => {
-    table.string('publisherId', 40);
+    table
+      .string('publisherId', 40)
+      .defaultTo(1)
+      .notNullable();
     table.foreign('publisherId').references('publishers.id');
   });
+
+  await knex('publisher_custom_values').insert([
+    {
+      name: 's2cor__Sage_ACC_Journal__c.s2cor__Reference__c',
+      publisherId: '1',
+      value: 'Wiley-Hindawi APC Recognition for article'
+    },
+    {
+      name: 's2cor__Sage_ACC_Journal__c.s2cor__Reference__c',
+      publisherId: '2',
+      value: 'Hindawi APC Recognition for article'
+    },
+    {
+      name: 's2cor__Sage_ACC_Journal_Item__c.s2cor__Reference__c',
+      publisherId: '1',
+      value: 'Hindawi APC Recognition for article'
+    },
+    {
+      name: 's2cor__Sage_ACC_Journal_Item__c.s2cor__Reference__c',
+      publisherId: '2',
+      value: 'Wiley-Hindawi APC Recognition for article'
+    },
+    {
+      name: 's2cor__Sage_ACC_Journal_Tag__c.s2cor__Tag__c',
+      publisherId: '1',
+      value: 'a5L0Y000000g0EeUAI'
+    },
+    {
+      name: 's2cor__Sage_ACC_Journal_Tag__c.s2cor__Tag__c',
+      publisherId: '2',
+      value: 'a5L0Y000000g0TMUAY'
+    },
+    {
+      name: 's2cor__Sage_ACC_Journal_Item_Tag__c.s2cor__Tag__c',
+      publisherId: '1',
+      value: 'a5L0Y000000PFE7UAO'
+    },
+    {
+      name: 's2cor__Sage_ACC_Journal_Item_Tag__c.s2cor__Tag__c',
+      publisherId: '2',
+      value: 'a5L0Y000000fzUOUAY'
+    },
+    {
+      name: 'S2cor__Sage_INV_Trade_Document_Item__c.s2cor__Product__c',
+      publisherId: '1',
+      value: '01t0Y000002BuB9QAK'
+    },
+    {
+      name: 'S2cor__Sage_INV_Trade_Document_Item__c.s2cor__Product__c',
+      publisherId: '2',
+      value: '01t0Y000002BkzAQAS'
+    }
+  ]);
 };
 
 module.exports.down = async function(knex) {
