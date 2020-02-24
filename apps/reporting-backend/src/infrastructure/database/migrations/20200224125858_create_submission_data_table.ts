fa808085bb3b5b5088620ed8b48f310a4cb6478f
Raul Popovici <raulpopovici@Rauls-MacBook-Pro-2.local>
2020-02-24 17:23:42 +0200

    add submission data indexes

--- apps/reporting-backend/src/infrastructure/database/migrations/20200224125858_create_submission_data_table.ts
+++ apps/reporting-backend/src/infrastructure/database/migrations/20200224125858_create_submission_data_table.ts
@@ -4,10 +4,20 @@ import { submissionDataView } from '../../views';
 export async function up(knex: Knex): Promise<any> {
   await knex.raw(submissionDataView.getCreateQuery());
   await knex.raw(submissionDataView.getTriggerQuery());
+  await Promise.all(
+    submissionDataView
+      .getPostCreateQueries()
+      .map(indexQuery => knex.raw(indexQuery))
+  );
 }
 
 export async function down(knex: Knex): Promise<any> {
-  knex.raw(`DROP table IF EXISTS ${submissionDataView.getViewName()} cascade`);
+  await knex.raw(
+    `DROP table IF EXISTS ${submissionDataView.getViewName()} cascade`
+  );
+  await knex.raw(
+    `DROP TRIGGER ${submissionDataView.getTriggerName()} ON public.submission_events;  `
+  );
 }
 
 export const name = '20200224125858_create_submission_data_table.ts';
