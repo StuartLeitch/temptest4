5d3e9f33f54937ebe889836ddde7f8f1221d7c93
Raul Popovici <raulpopovici@Rauls-MacBook-Pro-2.local>
2020-02-28 11:07:22 +0200

    add unknown type to scheduler

--- libs/shared/src/lib/infrastructure/scheduler/Scheduler.ts
+++ libs/shared/src/lib/infrastructure/scheduler/Scheduler.ts
@@ -2,6 +2,6 @@ import { Job } from '../message-queues/contracts/Job';
 import { ScheduleTimer } from '../message-queues/contracts/Time';
 
 export interface SchedulerContract {
-  schedule(job: Job, queue: string, timer: ScheduleTimer): Promise<any>;
+  schedule(job: Job, queue: string, timer: ScheduleTimer): Promise<unknown>;
   removeRepeatable(jobId: string): Promise<any>;
 }
