e5d4bebc7bf9a86614e876e1424ccb7456765a7e
Lucian Lature <lucian.lature@hindawi.com>
2020-03-02 14:58:39 +0200

    Dance, shake, move.

--- apps/invoicing-graphql/src/loaders/schedulerLoader.ts
+++ apps/invoicing-graphql/src/loaders/schedulerLoader.ts
@@ -76,13 +76,10 @@ export const schedulerLoader: MicroframeworkLoader = async (
       },
       // TODO Describe second job
       async () => {
-        try {
-          const response = await retryRevenueRecognizedInvoicesToErpUsecase.execute();
-          if (response.isLeft()) {
-            throw response.value.error;
-          }
-        } catch (error) {
-          logger.error(error);
+        const response = await retryRevenueRecognizedInvoicesToErpUsecase.execute();
+        if (response.isLeft()) {
+          logger.error(response.value.errorValue().message);
+          throw response.value.error;
         }
       }
     ];
