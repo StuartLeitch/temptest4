c3218240436f52b19400ca4d071b74601c69e4ce
Rares Stan <rares.stan@hindawi.com>
2020-03-23 17:37:37 +0200

    Reorder the error types from response for send credit control reminder

--- libs/shared/src/lib/modules/notifications/usecases/sendInvoiceCreditControlReminder/sendInvoiceCreditControlReminderResponse.ts
+++ libs/shared/src/lib/modules/notifications/usecases/sendInvoiceCreditControlReminder/sendInvoiceCreditControlReminderResponse.ts
@@ -4,16 +4,16 @@ import { AppError } from '../../../../core/logic/AppError';
 import { SendInvoiceCreditControlReminderErrors } from './sendInvoiceCreditControlReminderErrors';
 
 export type SendInvoiceCreditControlReminderResponse = Either<
-  | SendInvoiceCreditControlReminderErrors.EmailSendingFailure
-  | SendInvoiceCreditControlReminderErrors.InvoiceIdRequiredError
-  | SendInvoiceCreditControlReminderErrors.InvoiceNotFoundError
   | SendInvoiceCreditControlReminderErrors.ManuscriptCustomIdRequiredError
-  | SendInvoiceCreditControlReminderErrors.NotificationDbSaveError
   | SendInvoiceCreditControlReminderErrors.RecipientEmailRequiredError
   | SendInvoiceCreditControlReminderErrors.RecipientNameRequiredError
-  | SendInvoiceCreditControlReminderErrors.RescheduleTaskFailed
   | SendInvoiceCreditControlReminderErrors.SenderEmailRequiredError
+  | SendInvoiceCreditControlReminderErrors.NotificationDbSaveError
   | SendInvoiceCreditControlReminderErrors.SenderNameRequiredError
+  | SendInvoiceCreditControlReminderErrors.InvoiceIdRequiredError
+  | SendInvoiceCreditControlReminderErrors.InvoiceNotFoundError
+  | SendInvoiceCreditControlReminderErrors.RescheduleTaskFailed
+  | SendInvoiceCreditControlReminderErrors.EmailSendingFailure
   | AppError.UnexpectedError,
   Result<void>
 >;
