e66abf819e74451508a8466855e0bc85b8f3b14c
Lucian Ailenei <lucian.ailenei@hindawi.com>
2020-04-24 18:36:00 +0300

    feat: coupons list page

--- apps/invoicing-admin/src/routes/Coupons/List/List.js
+++ apps/invoicing-admin/src/routes/Coupons/List/List.js
@@ -1,7 +1,40 @@
 import React from 'react';
 
 const CouponsList = () => {
-  return <div>Start</div>;
+  return (
+    <Card className='mb-0'>
+      <div className='table-responsive-xl'>
+        <Table className='mb-0 table-striped' hover>
+          <thead>
+            <tr>
+              {/* <th className='align-middle bt-0'>#</th> */}
+              <th className='align-middle bt-0'>Status</th>
+              <th className='align-middle bt-0'>Reference</th>
+              <th className='align-middle bt-0'>Manuscript Custom ID</th>
+              <th className='align-middle bt-0'>Issue Date</th>
+              <th className='align-middle bt-0'>APC</th>
+              <th className='align-middle bt-0'>Journal Title</th>
+              <th className='align-middle bt-0'>Manuscript Title</th>
+              <th className='align-middle bt-0'>Manuscript Acceptance Date</th>
+            </tr>
+          </thead>
+          <tbody>
+            <TrTableInvoicesList invoices={data?.invoices?.invoices || []} />
+          </tbody>
+        </Table>
+      </div>
+
+      <CardFooter className='d-flex justify-content-center pb-0'>
+        <ListPagination
+          totalRecords={data?.invoices?.totalCount}
+          pageNeighbours={1}
+          onPageChanged={onPageChanged}
+          pageLimit={pagination.limit}
+          currentPage={pagination.offset + 1}
+        />
+      </CardFooter>
+    </Card>
+  );
 };
 
 export default CouponsList;
