949cd0c1632be07fe5763973e674b6d4dc98e9d0
Lucian Ailenei <lucian.ailenei@hindawi.com>
2020-04-27 12:15:52 +0300

    feat: coupons list table

--- apps/invoicing-admin/src/routes/Coupons/List/List.js
+++ apps/invoicing-admin/src/routes/Coupons/List/List.js
@@ -1,5 +1,7 @@
 import React from 'react';
 
+import { Card, Table, Col, CardFooter } from '../../../components';
+
 const CouponsList = () => {
   return (
     <Card className='mb-0'>
@@ -7,31 +9,29 @@ const CouponsList = () => {
         <Table className='mb-0 table-striped' hover>
           <thead>
             <tr>
-              {/* <th className='align-middle bt-0'>#</th> */}
+              <th className='align-middle bt-0'>Coupon Type</th>
+              <th className='align-middle bt-0'>Code</th>
               <th className='align-middle bt-0'>Status</th>
-              <th className='align-middle bt-0'>Reference</th>
-              <th className='align-middle bt-0'>Manuscript Custom ID</th>
-              <th className='align-middle bt-0'>Issue Date</th>
-              <th className='align-middle bt-0'>APC</th>
-              <th className='align-middle bt-0'>Journal Title</th>
-              <th className='align-middle bt-0'>Manuscript Title</th>
-              <th className='align-middle bt-0'>Manuscript Acceptance Date</th>
+              <th className='align-middle bt-0'>Redeem Count</th>
+              <th className='align-middle bt-0'>Invoice Item Type</th>
+              <th className='align-middle bt-0'>Date Created</th>
+              <th className='align-middle bt-0'>Date Updated</th>
+              <th className='align-middle bt-0'>Name</th>
             </tr>
           </thead>
           <tbody>
-            <TrTableInvoicesList invoices={data?.invoices?.invoices || []} />
           </tbody>
         </Table>
       </div>
 
       <CardFooter className='d-flex justify-content-center pb-0'>
-        <ListPagination
+        {/* <ListPagination
           totalRecords={data?.invoices?.totalCount}
           pageNeighbours={1}
           onPageChanged={onPageChanged}
           pageLimit={pagination.limit}
           currentPage={pagination.offset + 1}
-        />
+        /> */}
       </CardFooter>
     </Card>
   );
