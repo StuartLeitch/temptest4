8853c65b2a87145f433e371a6bf245dfa848a68f
Lucian Ailenei <lucian.ailenei@hindawi.com>
2020-04-30 12:50:49 +0300

    fix: hide list when loading

--- apps/invoicing-admin/src/routes/components/Loading/Loading.tsx
+++ apps/invoicing-admin/src/routes/components/Loading/Loading.tsx
@@ -3,11 +3,15 @@ import LoadingOverlay from 'react-loading-overlay';
 
 import { Spinner } from '../../../components';
 
-export default ({ loading = true, children }) => (
+// can be either used as a HOC or individual component
+export default ({ loading = true, children = null }) => (
   <LoadingOverlay
     active={loading}
     spinner={
-      <Spinner style={{ width: '12em', height: '12em'}} color='primary' />
+      <Spinner
+        style={{ width: '12em', height: '12em' }}
+        color='primary'
+      />
     }
   >
     {children}
