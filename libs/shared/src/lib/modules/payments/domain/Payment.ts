866311cfbbb03c84bbcb1116e5ba4bacd9fde609
Lucian Lature <lucian.lature@hindawi.com>
2020-05-21 11:19:45 +0300

    Revert "Add Payment statuses."
    This reverts commit 5386574f4011099ed05d8f61f971222c3458f9bc.

--- libs/shared/src/lib/modules/payments/domain/Payment.ts
+++ libs/shared/src/lib/modules/payments/domain/Payment.ts
@@ -1,21 +1,15 @@
 // * Core Domain
-import { AggregateRoot } from '../../../core/domain/AggregateRoot';
-import { UniqueEntityID } from '../../../core/domain/UniqueEntityID';
-import { Result } from '../../../core/logic/Result';
-import { File } from '../../../domain/File';
-import { Amount } from '../../../domain/Amount';
+import {AggregateRoot} from '../../../core/domain/AggregateRoot';
+import {UniqueEntityID} from '../../../core/domain/UniqueEntityID';
+import {Result} from '../../../core/logic/Result';
+import {File} from '../../../domain/File';
+import {Amount} from '../../../domain/Amount';
 
 // * Subdomain
-import { InvoiceId } from '../../invoices/domain/InvoiceId';
-import { PayerId } from '../../payers/domain/PayerId';
-import { PaymentId } from './PaymentId';
-import { PaymentMethodId } from './PaymentMethodId';
-
-export enum PaymentStatus {
-  PENDING = 'PENDING',
-  FAILED = 'FAILED',
-  COMPLETED = 'FINAL',
-}
+import {InvoiceId} from '../../invoices/domain/InvoiceId';
+import {PayerId} from '../../payers/domain/PayerId';
+import {PaymentId} from './PaymentId';
+import {PaymentMethodId} from './PaymentMethodId';
 
 interface PaymentProps {
   invoiceId: InvoiceId;
@@ -74,7 +68,7 @@ export class Payment extends AggregateRoot<PaymentProps> {
   ): Result<Payment> {
     const payment = new Payment(
       {
-        ...props,
+        ...props
       },
       id
     );
