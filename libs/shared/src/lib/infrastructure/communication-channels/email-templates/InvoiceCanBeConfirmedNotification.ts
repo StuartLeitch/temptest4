66512df0a754ffb71c35fdbb8d44e4d84658f859
George Poenariu <george@Georges-MacBook-Pro.local>
2020-05-25 17:12:43 +0300

    feat: update APC email template

--- libs/shared/src/lib/infrastructure/communication-channels/email-templates/InvoiceCanBeConfirmedNotification.ts
+++ libs/shared/src/lib/infrastructure/communication-channels/email-templates/InvoiceCanBeConfirmedNotification.ts
@@ -24,50 +24,56 @@ export class InvoiceCanBeConfirmedNotificationTemplate {
       invoiceLink,
       manuscript,
       publisherName,
-      invoice
+      invoice,
     } = data;
     const subject = `${manuscript.customId}: Article Processing Charges`;
     const paragraph = `
     <h4>Thank you for choosing ${publisherName} to publish your manuscript</h4>
-    As an open access journal, publication of articles in ${
+    We are pleased to inform you that your manuscript, ${
+      manuscript.title
+    }, has been accepted for publication in the journal, ${
       catalogItem.journalTitle
-    } is associated with Article Processing Charges that amount to ${
+    }. We will be in touch shortly to request electronic files for your manuscript.
+    <br/>
+    The publication process will begin upon the receipt of these files.
+    <br/> <br/>
+    As an open access journal, ${
+      catalogItem.journalTitle
+    } has an associated Article Processing Charge of ${catalogItem.currency} ${
       catalogItem.amount
-    } ${catalogItem.currency}. The total charges for your manuscript ${
+    }. The total charges for your manuscript ${
       manuscript.customId
-    }, before any taxes, are ${invoiceItem.price} ${catalogItem.currency}.
-    ${
-      publisherName === 'Hindawi'
-        ? `If you are required to pay United Kingdom Value Added Tax (VAT) as an individual or institution this will increase the overall cost of the charges by the prevailing rate of UK VAT at the date of the invoice.`
-        : ''
-    }
-    <br/><h4>What to do next?</h4>
-    In order to finalize the invoice with the correct billing details, you now access the invoice to confirm the details. Invoices can be charged to an individual or an institution depending on who is paying. This invoice is payable upon receipt. You can access the invoice for your article and make payment through the following URL:
-    <br /><br />
+    }, before any taxes, is ${catalogItem.currency} ${invoiceItem.price}.
+    <br/> <br/>
+    VAT charges may apply depending on the country of the individual or institution being billed. Individuals resident in the UK or an EU Member state will be charged VAT. Institutions registered in the UK or an EU Member state should also provide their VAT number when completing the payer information. VAT is calculated at the applicable rate, currently 20%, on the net ${
+      catalogItem.currency
+    } amount and this VAT charge will be available for review on the invoice prior to confirmation.
+    <br/> <br/>
+    <h4>What to do next?</h4>
+    This invoice is payable upon receipt. You can view the invoice for your article, confirm billing details, apply coupons and make payment through the following URL:
+    <br /><br /><br />
 
     ${invoiceLink}
-
-    You do not need to login to your account to access the link. After entering your billing address information, you will be able to pay directly by debit or credit, PayPal or bank transfer.
-    We are unable to accept payment by cheque.
+    <br />
+    You do not need to log in to your account to access the link. After entering your billing address information, you will be able to pay by credit card, PayPal or bank transfer. We are unable to accept payment by check.
     <br /><br />
-    If paying by debit or credit card, we accept Visa, Mastercard, Discover and Maestro. You may find there are other card options if using your card against payment through PayPal.
+    If paying by <b>credit card</b>, we accept <b>Visa</b>, <b>Mastercard</b>, <b>Discover</b> and <b>Maestro</b>. If your credit card is not one of these then you may be able to use <b>PayPal</b> to make payment using your credit card.
     <br /><br />
-    If paying by bank transfer, please use invoice number <strong>${
+    If paying by <b>bank transfer</b>, please use invoice number <strong>${
       invoice.invoiceNumber
     }/${(
       invoice.dateAccepted || invoice.dateCreated
-    ).getFullYear()}</strong> in the payment reference and return a scanned copy of the payment authorization by email to <a href="mailto:${bankTransferCopyReceiverAddress}">${bankTransferCopyReceiverAddress}</a> to facilitate our tracking of your payment and to avoid us unnecessarily chasing payment.
+    ).getFullYear()}</strong> in the payment reference and return a scanned copy of the bank payment authorization by email to <a href="mailto:${bankTransferCopyReceiverAddress}">${bankTransferCopyReceiverAddress}</a> to help us track your payment. Please note that bank transfer payments can take up to a week to arrive and will be confirmed as soon as funds have cleared.
     <br /><br />
-    Please note that bank transfer payments can take up to a week to arrive and will be confirmed as soon as funds have cleared.
-    <br /><br />
-    If the payment will be made by an alternative source such as your institution, you can provide them with the invoice link.
+    If the payment needs to be made by your institution or by another author, you can forward them the invoice link in this email and they will be able to make payment directly. However, please note that as the author that submitted the article remains responsible for ensuring payment is made in full, you may be contacted if this institution or author does not make payment.
+    <br/>
     <h4>Got a question?</h4>
     If you have any questions related to the invoice, just reply to this email and our Customer Service team will be happy to help.
     `;
 
     return {
       paragraph,
-      subject
+      subject,
     };
   }
 }
