9e4851df278263d3a26c3314bcc728092688f901
Raul Popovici <raul.popovici95@gmail.com>
2020-06-30 19:37:20 +0300

    docker login fix

--- tools/scripts/docker-login.sh
+++ tools/scripts/docker-login.sh
@@ -1,6 +1,1 @@
-# aws cli v2
-aws ecr get-login-password \
-    --region $AWS_REGION \
-  | docker login \
-    --username AWS \
-    --password-stdin $AWS_REGISTRY
+$(aws ecr get-login --no-include-email --region "${AWS_REGION}")
