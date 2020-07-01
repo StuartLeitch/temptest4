f6791d4ba8acec52d1d0dc0c6720ab4ce8b398f2
Raul Popovici <raul.popovici95@gmail.com>
2020-07-01 12:59:49 +0300

    add invoicing apps

--- tools/scripts/dockerize-apps.sh
+++ tools/scripts/dockerize-apps.sh
@@ -5,8 +5,8 @@ do
     echo "Building dist/apps/${APP}/Dockerfile"
     docker build --build-arg BUILD_IMAGE=$BUILD_IMAGE --no-cache -f dist/apps/$APP/Dockerfile -t $AWS_REGISTRY/$APP:$CI_COMMIT_SHA .
     docker push $AWS_REGISTRY/$APP:$CI_COMMIT_SHA
-    docker tag $AWS_REGISTRY/$APP:$CI_COMMIT_SHA $AWS_REGISTRY/$APP:ci-test
-    docker push $AWS_REGISTRY/$APP:ci-test
+    # TODO check for git tag docker tag $AWS_REGISTRY/$APP:$CI_COMMIT_SHA $AWS_REGISTRY/$APP:$CI_COMMIT_TAG
+    # docker push $AWS_REGISTRY/$APP:$CI_COMMIT_TAG
   else
     echo "Application ${APP} doesn't build a docker image. SKIPPING."
   fi
