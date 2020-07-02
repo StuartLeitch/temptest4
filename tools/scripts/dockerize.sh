9d7347d66751fe35688a8b39ae93edc8ded2c732
Raul Popovici <raul.popovici95@gmail.com>
2020-07-02 14:03:20 +0300

    build and package only on dev

--- tools/scripts/dockerize.sh
+++ tools/scripts/dockerize.sh
@@ -7,8 +7,8 @@ do
     echo "Building dist/apps/${APP}/Dockerfile"
     docker build -f dist/apps/$APP/Dockerfile -t $AWS_REGISTRY/$APP:$CI_COMMIT_SHA .
     docker push $AWS_REGISTRY/$APP:$CI_COMMIT_SHA
-    docker tag $AWS_REGISTRY/$APP:$CI_COMMIT_SHA $AWS_REGISTRY/$APP:ci-test
-    docker push $AWS_REGISTRY/$APP:ci-test
+    docker tag $AWS_REGISTRY/$APP:$CI_COMMIT_SHA $AWS_REGISTRY/$APP:dev
+    docker push $AWS_REGISTRY/$APP:dev
   else
     echo "Application ${APP} doesn't build a docker image. SKIPPING."
   fi
