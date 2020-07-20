6f947423207fc524accf281995282202222ba8b7
Lucian Lature <lucian.lature@hindawi.com>
2020-07-20 13:27:48 +0300

    Code clean-up.

--- libs/shared/src/lib/domain/services/sqs/connection.ts
+++ libs/shared/src/lib/domain/services/sqs/connection.ts
@@ -1,15 +1,5 @@
 import { createQueueService } from '@hindawi/queue-service';
 
-// import { environment } from '@env/environment';
-
-// const { AWS_SNS_SQS_REGION, AWS_SNS_ENDPOINT, AWS_SNS_TOPIC } = process.env;
-
-// const region = AWS_SNS_SQS_REGION;
-// const endpoint = AWS_SNS_ENDPOINT || process.env.AWS_SNS_ENDPOINT;
-// const topic = AWS_SNS_TOPIC || process.env.AWS_SNS_TOPIC;
-
-console.log('BOOOHOOO');
-
 const sqsConnection = createQueueService(); // creates a new client
 
 export { sqsConnection };
