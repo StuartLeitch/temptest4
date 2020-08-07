25ef2ef15a3e14c26a3c3543435cb3b69b20f2dc
Lucian Lature <lucian.lature@hindawi.com>
2020-08-07 11:06:45 +0300

    Fix the missing migrations scripts.

---
+++ libs/shared/src/lib/infrastructure/database/knex/migrations/20200618145121_add_migration_payment_method.js
@@ -0,0 +1,23 @@
+/* eslint-disable */
+const { v4 } = require('uuid');
+
+module.exports.up = async function (knex) {
+  const alreadyExists = await knex('payment_methods')
+    .count('id')
+    .where({ name: 'Migration' })
+    .first();
+
+  if (alreadyExists.count === 0) {
+    return knex('payment_methods').insert({
+      id: v4(),
+      name: 'Migration',
+      isActive: false,
+    });
+  }
+
+  return null;
+};
+
+module.exports.down = function (knex) {
+  return null;
+};
