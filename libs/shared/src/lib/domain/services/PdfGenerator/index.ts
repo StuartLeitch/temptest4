bc6036edafa88000186b6da25f74821a0652b88d
Silviu Popovici <silviu.popovici94@gmail.com>
2020-08-26 12:26:10 +0300

    fixed CSS and added Logger on Generator service index

--- libs/shared/src/lib/domain/services/PdfGenerator/index.ts
+++ libs/shared/src/lib/domain/services/PdfGenerator/index.ts
@@ -1,8 +1,11 @@
 import { PdfGeneratorService } from './PdfGenerator';
 import type { InvoicePayload } from './PdfGenerator';
+import { LoggerContract } from '../../../infrastructure/logging/Logger';
 
 export { PdfGeneratorService, InvoicePayload };
 
-export const pdfGeneratorService = new PdfGeneratorService();
+let logger: LoggerContract;
+
+export const pdfGeneratorService = new PdfGeneratorService(logger);
 
 pdfGeneratorService.addTemplate('invoice', 'invoice.ejs');
