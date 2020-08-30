1b0db3d09e1e7346e16814fa221c43dc9d7438df
Rares Stan <rares.stan@hindawi.com>
2020-08-30 14:00:25 +0300

    [invoicing-web] - feat: Change "Arxiv ID" to "Preprint ID"

--- apps/invoicing-web/src/app/pages/PaymentDetails/Details/ArticleDetails/ArticleDetails.tsx
+++ apps/invoicing-web/src/app/pages/PaymentDetails/Details/ArticleDetails/ArticleDetails.tsx
@@ -30,7 +30,7 @@ const ArticleDetails: React.FunctionComponent<Props> = ({ article }: any) => {
           label="Corresponding Author"
           text={`${article.authorFirstName} ${article.authorSurname}`}
         />
-        <DetailItem label="Arxiv ID" text={article.arxivId} />
+        <DetailItem label="Preprint ID" text={article.arxivId} />
       </Root>
     </Expander>
   );
