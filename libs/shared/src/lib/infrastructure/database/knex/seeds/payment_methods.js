c90a8c518612fe7073a4a9a2ef2cc479912e231b
Lucian Lature <lucian.lature@hindawi.com>
2020-09-21 21:22:16 +0300

    Fix some issues in seeding.

--- libs/shared/src/lib/infrastructure/database/knex/seeds/payment_methods.js
+++ libs/shared/src/lib/infrastructure/database/knex/seeds/payment_methods.js
@@ -1,30 +1,30 @@
 /* eslint-disable */
-const { v4 } = require('uuid');
+const uuid = require('uuid/v4');
 
 const paymentMethods = [
   {
-    id: v4(),
+    id: uuid(),
     name: 'Credit Card',
     isActive: true,
   },
   {
-    id: v4(),
+    id: uuid(),
     name: 'Bank Transfer',
     isActive: true,
   },
   {
-    id: v4(),
+    id: uuid(),
     name: 'Paypal',
     isActive: true,
   },
 ];
 
 const seed = async (knex) => {
-  // console.log('Seeding payment methods...');
-  // await knex('payment_methods').truncate();
-  // await Promise.all(
-  //   paymentMethods.map((pm) => knex('payment_methods').insert(pm))
-  // );
+  console.log('Seeding payment methods...');
+  await knex('payment_methods').truncate();
+  await Promise.all(
+    paymentMethods.map((pm) => knex('payment_methods').insert(pm))
+  );
 };
 
 exports.seed = seed;
