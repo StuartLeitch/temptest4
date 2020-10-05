11dcf3924d1c459b2cf119018d5433bb4f2542e3
Silviu Popovici <silviu.popovici94@gmail.com>
2020-10-05 11:23:09 +0300

    mismatch between MockEvent and Event from s3-event-producer

--- libs/eve/src/lib/core/producer/implementations/s3-event/s3-event-producer.spec.ts
+++ libs/eve/src/lib/core/producer/implementations/s3-event/s3-event-producer.spec.ts
@@ -3,28 +3,28 @@ import { Selector } from '../../../selector';
 import { Filter } from '../../../filters';
 
 interface MockEvent {
-  messageId: string;
-  body: string;
+  MessageId: string;
+  Message: string;
 }
 
 let S3;
 
 const mockKeys = [
   {
-    Key: '1'
+    Key: '1',
   },
   {
-    Key: '2'
+    Key: '2',
   },
   {
-    Key: '3'
-  }
+    Key: '3',
+  },
 ];
 
 const mockObjects = {
-  '1': `{"messageId":"1","body":"{'a':'1'}"}`,
-  '2': `{"messageId":"2","body":"{'a':'2'}"}`,
-  '3': `{"messageId":"3","body":"{'a':'3'}"}`
+  '1': `{"MessageId":"1","Message":"{'a':'1'}"}`,
+  '2': `{"MessageId":"2","Message":"{'a':'2'}"}`,
+  '3': `{"MessageId":"3","Message":"{'a':'3'}"}`,
 };
 
 class MockSelector implements Selector<string> {
@@ -50,7 +50,7 @@ class MockFilter implements Filter<MockEvent> {
   }
 
   match(event: MockEvent): boolean {
-    return this.acceptedEvents.includes(event.messageId);
+    return this.acceptedEvents.includes(event.MessageId);
   }
 }
 
@@ -61,10 +61,10 @@ describe('S3 Event Producer', () => {
         IsTruncated: false,
         $response: {
           data: {
-            Contents: mockKeys
-          }
-        }
-      })
+            Contents: mockKeys,
+          },
+        },
+      }),
     });
     const s3GetObject = jest.fn(({ Key }) => {
       const values = mockObjects;
@@ -72,15 +72,15 @@ describe('S3 Event Producer', () => {
         promise: jest.fn().mockResolvedValue({
           $response: {
             data: {
-              Body: values[Key]
-            }
-          }
-        })
+              Message: values[Key],
+            },
+          },
+        }),
       };
     });
     S3 = jest.fn().mockImplementation(() => ({
       listObjectsV2: s3ListObjectKeysV2,
-      getObject: s3GetObject
+      getObject: s3GetObject,
     }));
   });
 
@@ -88,17 +88,19 @@ describe('S3 Event Producer', () => {
     const producer = new S3EventProducer(S3(), '');
     const list = producer.produce();
     const expectedResult = [
-      { messageId: '1', body: "{'a':'1'}" },
-      { messageId: '2', body: "{'a':'2'}" },
-      { messageId: '3', body: "{'a':'3'}" }
+      { MessageId: '1', Message: "{'a':'1'}" },
+      { MessageId: '2', Message: "{'a':'2'}" },
+      { MessageId: '3', Message: "{'a':'3'}" },
     ];
 
     const results = [];
     for await (const event of list) {
       results.push(event);
     }
-    expect(results.map(e => ({ ...e, body: e.body.replace(' ', '') }))).toEqual(
-      expectedResult.map(e => ({ ...e, body: e.body.replace(' ', '') }))
+    expect(
+      results.map((e) => ({ ...e, Message: e.Message.replace(' ', '') }))
+    ).toEqual(
+      expectedResult.map((e) => ({ ...e, Message: e.Message.replace(' ', '') }))
     );
   });
 
@@ -125,8 +127,8 @@ describe('S3 Event Producer', () => {
 
     const list = producer.produce();
     const expectedResult = [
-      { messageId: '1', body: "{'a':'1'}" },
-      { messageId: '3', body: "{'a':'3'}" }
+      { MessageId: '1', Message: "{'a':'1'}" },
+      { MessageId: '3', Message: "{'a':'3'}" },
     ];
 
     const results = [];
@@ -134,8 +136,10 @@ describe('S3 Event Producer', () => {
       results.push(event);
     }
 
-    expect(results.map(e => ({ ...e, body: e.body.replace(' ', '') }))).toEqual(
-      expectedResult.map(e => ({ ...e, body: e.body.replace(' ', '') }))
+    expect(
+      results.map((e) => ({ ...e, Message: e.Message.replace(' ', '') }))
+    ).toEqual(
+      expectedResult.map((e) => ({ ...e, Message: e.Message.replace(' ', '') }))
     );
     expect(s3.getObject.mock.calls[0][0].Key).toBe('1');
     expect(s3.getObject.mock.calls[1][0].Key).toBe('2');
