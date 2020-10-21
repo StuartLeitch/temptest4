e3821a317505b2e455a8901eaef0a7b3e34745c5
Silviu Popovici <silviu.popovici94@gmail.com>
2020-10-21 13:34:58 +0300

    added errors to usecase response

--- libs/shared/src/lib/modules/transactions/usecases/restoreSoftDeleteDraftTransaction/restoreSoftDeleteDraftTransaction.response.ts
+++ libs/shared/src/lib/modules/transactions/usecases/restoreSoftDeleteDraftTransaction/restoreSoftDeleteDraftTransaction.response.ts
@@ -1,8 +1,17 @@
 import { Either, Result } from '../../../../core/logic/Result';
 import { UnexpectedError } from '../../../../core/logic/AppError';
+import * as Errors from './restoreSoftDeleteDraftTransaction.errors';
 
 export type RestoreSoftDeleteDraftTransactionResponse = Either<
-  // | SoftDeleteDraftTransactionErrors.TransactionNotFoundError
-  UnexpectedError,
+  | Errors.InvoiceRestoreError
+  | Errors.TransactionNotFoundError
+  | Errors.InvoiceNotFoundError
+  | Errors.InvoiceItemNotFoundError
+  | Errors.ManuscriptNotFoundError
+  | Errors.InvoiceItemRestoreError
+  | Errors.InvoiceRestoreError
+  | Errors.ManuscriptRestoreError
+  | Errors.TransactionRestoreError
+  | UnexpectedError,
   Result<void>
 >;
