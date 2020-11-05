88ef2005fbbf76c4152da73491f95cb48c0886b1
Lucian Lature <lucian.lature@hindawi.com>
2020-11-05 14:24:26 +0200

    refactor(shared): :zap: Fix some small issues.
    Fix small issues.

--- libs/shared/src/lib/infrastructure/database/knex/migrations/20201006093954_add_erpid_to_payments_table.js
+++ libs/shared/src/lib/infrastructure/database/knex/migrations/20201006093954_add_erpid_to_payments_table.js
@@ -2,12 +2,12 @@
 
 module.exports.up = function (knex) {
   return knex.schema.table('payments', function (table) {
-    table.string('foreignPaymentId', 40);
+    table.string('erpId', 40);
   });
 };
 
 module.exports.down = function (knex) {
   return knex.schema.table('payments', function (table) {
-    table.dropColumn('foreignPaymentId');
+    table.dropColumn('erpId');
   });
 };
