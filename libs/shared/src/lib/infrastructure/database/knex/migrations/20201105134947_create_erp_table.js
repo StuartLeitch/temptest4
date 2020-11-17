3c6b1416b0eaf52ea5d938fe5ef0b5326f1a2b3c
Lucian Lature <lucian.lature@hindawi.com>
2020-11-17 18:24:18 +0200

    feat(shared): :white_check_mark: Fix a migratioon issue related to `erp_references` primary key.

--- libs/shared/src/lib/infrastructure/database/knex/migrations/20201105134947_create_erp_table.js
+++ libs/shared/src/lib/infrastructure/database/knex/migrations/20201105134947_create_erp_table.js
@@ -3,35 +3,14 @@
 
 module.exports.up = async function (knex) {
   await knex.schema.createTable('erp_references', (table) => {
-    table.string('entity_id', 40).primary();
+    table.string('entity_id', 40);
     table.string('type', 40);
     table.string('vendor', 40);
     table.string('attribute');
     table.string('value');
-    // table.unique('name');
   });
-
-  // await knex.schema.table('catalog', (table) => {
-  //   table.string('publisherId', 40).notNullable();
-  //   table.foreign('publisherId').references('publishers.id');
-  // });
-
-  // await knex.schema.createTable('publisher_custom_values', (table) => {
-  //   table.string('name');
-  //   table.string('publisherId', 40);
-  //   table.string('value');
-  //   table
-  //     .foreign('publisherId')
-  //     .references('publishers.id')
-  //     .onDelete('CASCADE');
-  //   table.primary(['name', 'publisherId']);
-  // });
 };
 
 module.exports.down = async function (knex) {
-  // await knex.schema.table('catalog', (table) => {
-  //   table.dropColumn('publisherId');
-  // });
-  // await knex.schema.dropTable('publisher_custom_values');
   await knex.schema.dropTable('erp_references');
 };
