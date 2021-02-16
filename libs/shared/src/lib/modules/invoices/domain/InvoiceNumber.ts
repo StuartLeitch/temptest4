8f1929626200a8b2c55629033eded8b1ffd4b87e
Lucian Lature <lucian.lature@hindawi.com>
2021-02-16 09:36:36 +0200

    Get rid of length limitation.

--- libs/shared/src/lib/modules/invoices/domain/InvoiceNumber.ts
+++ libs/shared/src/lib/modules/invoices/domain/InvoiceNumber.ts
@@ -7,8 +7,6 @@ export interface InvoiceNumberProps {
 }
 
 export class InvoiceNumber extends ValueObject<InvoiceNumberProps> {
-  public static minLength: number = 6;
-
   get value () : number {
     return this.props.value;
   }
@@ -17,25 +15,12 @@ export class InvoiceNumber extends ValueObject<InvoiceNumberProps> {
     super(props);
   }
 
-  private static isAppropriateLength (value: number): boolean {
-    return String(value).length >= this.minLength;
-  }
-
   public static create (props: InvoiceNumberProps): Result<InvoiceNumber> {
     const propsResult = Guard.againstNullOrUndefined(props.value, 'invoiceNumber');
 
     if (!propsResult.succeeded) {
       return Result.fail<InvoiceNumber>(propsResult.message);
     } else {
-
-      if (!props.value) {
-        if (
-          !this.isAppropriateLength(props.value)
-        ) {
-          return Result.fail<InvoiceNumber>('Invoice number doesn\'t meet criteria [6 chars min].');
-        }
-      }
-
       return Result.ok<InvoiceNumber>(new InvoiceNumber({
         value: props.value
       }));
