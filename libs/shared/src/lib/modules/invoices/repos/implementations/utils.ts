53daffb7de90d9dbbf33bc112dfcb125625fce47
Rares Stan <rares.stan@hindawi.com>
2021-03-11 14:00:58 +0200

    chore(shared/invoices): Use the persisted reference number when searching by reference number

--- libs/shared/src/lib/modules/invoices/repos/implementations/utils.ts
+++ libs/shared/src/lib/modules/invoices/repos/implementations/utils.ts
@@ -12,50 +12,48 @@ interface Filters {
 function applyJoins(src: QueryBuilder, filterEntries) {
   let here: QueryBuilder = src;
   const categorizedPaths = new Categories([
-    ["/invoiceItem/article/journalId", "/invoiceItem/article/customId"],
-    ["/transactionStatus"]
+    ['/invoiceItem/article/journalId', '/invoiceItem/article/customId'],
+    ['/transactionStatus'],
   ]);
 
   for (const [keyList] of filterEntries) {
     const path = toPath(keyList);
-    if (!categorizedPaths.has(path))
-      continue;
+    if (!categorizedPaths.has(path)) continue;
 
     categorizedPaths.dropCategoryOf(path);
 
     switch (path) {
-    case "/invoiceItem/article/journalId":
-    case "/invoiceItem/article/customId":
-      here = here
-        .join(
-          TABLES.INVOICE_ITEMS,
-          `${TABLES.INVOICES}.id`,
-          '=',
-          `${TABLES.INVOICE_ITEMS}.invoiceId`
-        )
-        .join(
-          TABLES.ARTICLES,
-          `${TABLES.ARTICLES}.id`,
-          '=',
-          `${TABLES.INVOICE_ITEMS}.manuscriptId`
-        );
+      case '/invoiceItem/article/journalId':
+      case '/invoiceItem/article/customId':
+        here = here
+          .join(
+            TABLES.INVOICE_ITEMS,
+            `${TABLES.INVOICES}.id`,
+            '=',
+            `${TABLES.INVOICE_ITEMS}.invoiceId`
+          )
+          .join(
+            TABLES.ARTICLES,
+            `${TABLES.ARTICLES}.id`,
+            '=',
+            `${TABLES.INVOICE_ITEMS}.manuscriptId`
+          );
         // .join(
         //   TABLES.CATALOG,
         //   `${TABLES.CATALOG}.journalId`,
         //   '=',
         //   `${TABLES.ARTICLES}.journalId`
         // )
-      break;
+        break;
 
-    case "/transactionStatus":
-      here = here
-        .join(
+      case '/transactionStatus':
+        here = here.join(
           TABLES.TRANSACTIONS,
           `${TABLES.INVOICES}.transactionId`,
           '=',
           `${TABLES.TRANSACTIONS}.id`
         );
-      break;
+        break;
     }
   }
 
@@ -69,29 +67,29 @@ export function applyFilters(src: QueryBuilder, filters: Filters) {
   let invoiceNumber: string, creationYear: string;
   for (const [keyList, filter] of cachedEntries) {
     switch (toPath(keyList)) {
-    case "/invoiceItem/article/journalId":
-      here = here.whereIn(`${TABLES.ARTICLES}.journalId`, filter);
-      //         .whereIn(`${TABLES.CATALOG}.id`, filter);
-      break;
+      case '/invoiceItem/article/journalId':
+        here = here.whereIn(`${TABLES.ARTICLES}.journalId`, filter);
+        //         .whereIn(`${TABLES.CATALOG}.id`, filter);
+        break;
 
-    case "/invoiceItem/article/customId":
-      here = here.whereIn(`${TABLES.ARTICLES}.customId`, filter);
-      break;
+      case '/invoiceItem/article/customId':
+        here = here.whereIn(`${TABLES.ARTICLES}.customId`, filter);
+        break;
 
-    case "/transactionStatus":
-      here = here.whereIn(`${TABLES.TRANSACTIONS}.status`, filter);
-      break;
+      case '/transactionStatus':
+        here = here.whereIn(`${TABLES.TRANSACTIONS}.status`, filter);
+        break;
 
-    case "/invoiceStatus":
-      here = here.whereIn(`${TABLES.INVOICES}.status`, filter);
-      break;
+      case '/invoiceStatus':
+        here = here.whereIn(`${TABLES.INVOICES}.status`, filter);
+        break;
 
-    case "/referenceNumber":
-      [invoiceNumber, creationYear] = ParseUtils.parseRefNumber(filter[0]);
-      here = here.whereRaw(
-        `"${TABLES.INVOICES}"."invoiceNumber" = ${invoiceNumber} and extract(year from "${TABLES.INVOICES}"."dateAccepted") = ${creationYear}`
-      );
-      break;
+      case '/referenceNumber':
+        const invoiceRef = filter[0];
+        here = here.whereRaw(
+          `"${TABLES.INVOICES}"."persistentReferenceNumber" = ${invoiceRef}`
+        );
+        break;
     }
   }
 
