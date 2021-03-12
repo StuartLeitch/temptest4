503e083a29924fc08cc18a2ad777ab4f6dc9ff55
Rares Stan <rares.stan@hindawi.com>
2021-03-12 19:33:22 +0200

    fix(shared/emails): Remove the invoice number from the sanctioned country email

--- libs/shared/src/lib/infrastructure/communication-channels/email-templates/InvoicePendingNotification.ts
+++ libs/shared/src/lib/infrastructure/communication-channels/email-templates/InvoicePendingNotification.ts
@@ -6,8 +6,7 @@ export class InvoicePendingNotificationTemplate {
   static build(invoice: Invoice): EmailContent {
     const subject = `[Sanctioned Country] An Invoice was confirmed from a Sanctioned Country`;
     const paragraph = `
-      The invoice with id {${invoice.id.toString()}} and reference number
-      {${invoice.persistentReferenceNumber}} has been confirmed from a Sanctioned Country.
+      The invoice with id {${invoice.id.toString()}} has been confirmed from a Sanctioned Country. Use path "/invoices/details/${invoice.id.toString()}" in the admin dashboard to access the details of this invoice.
     `;
 
     return {
