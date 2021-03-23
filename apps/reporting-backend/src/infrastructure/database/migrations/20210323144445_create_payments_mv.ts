54e0df9048511219e712d09ce23349d726a7aca8
Silviu Popovici <silviu.popovici94@gmail.com>
2021-03-23 15:46:41 +0200

    feat: fixed payments mv

--- apps/reporting-backend/src/infrastructure/database/migrations/20210323144445_create_payments_mv.ts
+++ apps/reporting-backend/src/infrastructure/database/migrations/20210323144445_create_payments_mv.ts
@@ -12,11 +12,11 @@ export async function up(knex: Knex): Promise<any> {
   // * payments is dependent on invoice_data
 
   const invoiceDataAlias = 'id';
-  const getViewName = 'invoices_data';
-  const getPartitionQuery = `partition by ${invoiceDataAlias}.invoice_id ORDER BY case when ${invoiceDataAlias}.status = 'FINAL' then 1 when ${invoiceDataAlias}.status = 'ACTIVE' then 2 else 3 end, ${invoiceDataAlias}.event_timestamp desc nulls last`;
+  const invoiceDataViewName = 'invoices_data';
+  const invoicePartitionQuery = `partition by ${invoiceDataAlias}.invoice_id ORDER BY case when ${invoiceDataAlias}.status = 'FINAL' then 1 when ${invoiceDataAlias}.status = 'ACTIVE' then 2 else 3 end, ${invoiceDataAlias}.event_timestamp desc nulls last`;
 
   await knex.raw(`
-  CREATE MATERIALIZED VIEW IF NOT EXISTS ${getViewName}
+  CREATE MATERIALIZED VIEW IF NOT EXISTS payments
 AS
 SELECT
   last_inv.event_id,
@@ -29,7 +29,7 @@ SELECT
   cast_to_timestamp(payment_view. "paymentDate") AS payment_date,
   payment_view."paymentAmount"::float AS payment_amount
 FROM (SELECT ie.payload, last_inv.*	
-  FROM (SELECT * FROM (SELECT *, ROW_NUMBER() OVER (${getPartitionQuery}) AS rn FROM ${getViewName} id) i
+  FROM (SELECT * FROM (SELECT *, ROW_NUMBER() OVER (${invoicePartitionQuery}) AS rn FROM ${invoiceDataViewName} id) i
     WHERE	rn = 1) last_inv
   JOIN ${REPORTING_TABLES.INVOICE} ie ON ie.id = last_inv.event_id
 ) last_inv, 
@@ -40,13 +40,13 @@ WITH NO DATA;
   `);
 
   const postCreateQueries = [
-    `CREATE index on payments (event_id)`,
-    `CREATE index on payments (invoice_id)`,
-    `CREATE index on payments (invoice_id, payment_date desc nulls last)`,
-    `CREATE index on payments (payment_date desc nulls last)`,
-    `CREATE index on payments (manuscript_custom_id)`,
-    `CREATE index on payments (reference_number)`,
-    `CREATE index on payments (payment_type)`,
+    `CREATE INDEX ON payments (event_id)`,
+    `CREATE INDEX ON payments (invoice_id)`,
+    `CREATE INDEX ON payments (invoice_id, payment_date desc nulls last)`,
+    `CREATE INDEX ON payments (payment_date desc nulls last)`,
+    `CREATE INDEX ON payments (manuscript_custom_id)`,
+    `CREATE INDEX ON payments (reference_number)`,
+    `CREATE INDEX ON payments (payment_type)`,
   ];
 
   for (const indexQuery of postCreateQueries) {
@@ -60,7 +60,7 @@ WITH NO DATA;
   }
 
   logger.info(
-    `Creating table and indices checker_to_submission took ${differenceInSeconds(
+    `Creating table and indices payments took ${differenceInSeconds(
       queryStart
     )} seconds`
   );
