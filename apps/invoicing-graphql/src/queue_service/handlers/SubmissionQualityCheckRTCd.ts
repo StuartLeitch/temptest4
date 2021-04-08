f400d9f4a44473b0c0d6a83ede224ca1f20b8750
Lucian Lature <lucian.lature@hindawi.com>
2021-04-08 13:50:26 +0300

    Add SubmissionScreeningVoid event.

--- apps/invoicing-graphql/src/queue_service/handlers/SubmissionQualityCheckRTCd.ts
+++ apps/invoicing-graphql/src/queue_service/handlers/SubmissionQualityCheckRTCd.ts
@@ -1,12 +1,12 @@
-import { SubmissionScreeningVoid as SubmissionScreeningVoidEvent } from '@hindawi/phenom-events';
+import { SubmissionQualityCheckRTCd as SubmissionQualityCheckRTCdEvent } from '@hindawi/phenom-events';
 
 import { SubmissionWithdrawn } from './SubmissionWithdrawn';
 
 import { EventHandler } from '../event-handler';
 
-const SUBMISSION_SCREENING_VOID = 'SubmissionScreeningVoid';
+const SUBMISSION_QUALITY_CHECK_RTCD = 'SubmissionQualityCheckRTCd';
 
-export const SubmissionQualityCheckRTCd: EventHandler<SubmissionScreeningVoidEvent> = {
-  event: SUBMISSION_SCREENING_VOID,
+export const SubmissionQualityCheckRTCd: EventHandler<SubmissionQualityCheckRTCdEvent> = {
+  event: SUBMISSION_QUALITY_CHECK_RTCD,
   handler: SubmissionWithdrawn.handler,
 };
