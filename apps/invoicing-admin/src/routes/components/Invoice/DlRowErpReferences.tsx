dc4772b36314e8c302e618150e211df3a703cecf
Silviu Popovici <silviu.popovici94@gmail.com>
2021-04-20 13:29:50 +0300

    style: erp ref column resizes

--- apps/invoicing-admin/src/routes/components/Invoice/DlRowErpReferences.tsx
+++ apps/invoicing-admin/src/routes/components/Invoice/DlRowErpReferences.tsx
@@ -4,36 +4,36 @@ import PropTypes from 'prop-types';
 const DlRowErpReferences = (props) => (
     <React.Fragment>
         <dl className='row'>
-            <dt className={`col-sm-5 ${props.leftSideClassName}`}>Invoice</dt>
-            <dd className={`col-sm-5 ${props.rightSideClassName}`}>
+            <dt className={`col-sm-4 ${props.leftSideClassName}`}>Invoice</dt>
+            <dd className={`col-sm-7 ${props.rightSideClassName}`}>
                 {props.erpReferences?.map(e => e.attribute === 'confirmation' ? <strong key='invoice' className='text-black-50 ml-5'>{e.value}</strong> : '' )}
             </dd>
         </dl>
 
         <dl className='row'>
-            <dt className={`col-sm-5 ${props.leftSideClassName}`}>Payment</dt>
-            <dd className={`col-sm-5 ${props.rightSideClassName}`}>
-            <dd className='ml-5'>{props.erpReferences?.map(e => e.attribute === 'payment' ? <strong key='payment' className='text-black-50 mr-3'>{e.value}</strong> : '' )}</dd>
+            <dt className={`col-sm-4 ${props.leftSideClassName}`}>Payment</dt>
+            <dd className={`col-sm-7 ${props.rightSideClassName}`}>
+            <div className='ml-5'>{props.erpReferences?.map(e => e.attribute === 'payment' ? <strong key='payment' className='text-black-50 mr-3'>{e.value}</strong> : '' )}</div>
             </dd>
         </dl>
 
         <dl className='row'>
-            <dt className={`col-sm-5 ${props.leftSideClassName}`}>Revenue Recognition</dt>
-            <dd className={`col-sm-5 ${props.rightSideClassName}`}>
+            <dt className={`col-sm-4 ${props.leftSideClassName}`}>Revenue Recognition</dt>
+            <dd className={`col-sm-7 ${props.rightSideClassName}`}>
                 {props.erpReferences?.map(e => e.attribute === 'revenueRecognition' ? <strong key='revenue-recognition' className='text-black-50 ml-5'>{e.value}</strong> : '' )}
             </dd>
         </dl>
 
         <dl className='row'>
-            <dt className={`col-sm-5 ${props.leftSideClassName}`}>Credit Note</dt>
-            <dd className={`col-sm-5 ${props.rightSideClassName}`}>
+            <dt className={`col-sm-4 ${props.leftSideClassName}`}>Credit Note</dt>
+            <dd className={`col-sm-7 ${props.rightSideClassName}`}>
                 {props.creditNote?.erpReferences?.map(e => e.attribute === 'creditNote' ? <strong key='credit-note' className='text-black-50 ml-5'>{e.value}</strong> : '' )}
             </dd>
         </dl>
 
         <dl className='row'>
-            <dt className={`col-sm-5 ${props.leftSideClassName}`}>Revenue Recognition Reversal</dt>
-            <dd className={`col-sm-5 ${props.rightSideClassName}`}>
+            <dt className={`col-sm-4 ${props.leftSideClassName}`}>Revenue Recognition Reversal</dt>
+            <dd className={`col-sm-7 ${props.rightSideClassName}`}>
                 {props.erpReferences?.map(e => e.attribute === 'revenueRecognitionReversal' ? <strong key='rev-rec-reversal' className='text-black-50 ml-5'>{e.value}</strong> : '' )}
             </dd>
         </dl>
