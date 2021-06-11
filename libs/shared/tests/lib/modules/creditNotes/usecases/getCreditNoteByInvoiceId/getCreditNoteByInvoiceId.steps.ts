d2ece4b2a1fbab675545880198a24a9e70052015
Silviu Popovici <silviu.popovici94@gmail.com>
2021-06-11 09:38:23 +0300

    test: added test for getCreditNoteByInvoiceIdUsecase

---
+++ libs/shared/tests/lib/modules/creditNotes/usecases/getCreditNoteByInvoiceId/getCreditNoteByInvoiceId.steps.ts
@@ -0,0 +1,59 @@
+import { expect } from 'chai';
+import { Before, After, Given, When, Then } from '@cucumber/cucumber';
+
+import {
+  Roles,
+  UsecaseAuthorizationContext,
+} from '../../../../../../../shared/src/lib/domain/authorization';
+
+import { CreditNote } from '../../../../../../src/lib/modules/creditNotes/domain/CreditNote';
+import { CreditNoteMap } from '../../../../../../src/lib/modules/creditNotes/mappers/CreditNoteMap';
+import { MockCreditNoteRepo } from '../../../../../../src/lib/modules/creditNotes/repos/mocks/mockCreditNoteRepo';
+import { GetCreditNoteByInvoiceIdUsecase } from '../../../../../../src/lib/modules/creditNotes/usecases/getCreditNoteByInvoiceId/getCreditNoteByInvoiceId';
+
+function makeCreditNoteData(overwrites?: any): CreditNote {
+  return CreditNoteMap.toDomain({
+    dateCreated: new Date(),
+    vat: 20,
+    price: -2000,
+    ...overwrites,
+  });
+}
+
+let mockCreditNoteRepo: MockCreditNoteRepo;
+let usecase: GetCreditNoteByInvoiceIdUsecase;
+let creditNote: CreditNote;
+let result: any;
+
+Before({ tags: '@ValidateGetCreditNoteByInvoiceIdUsecase' }, async () => {
+  mockCreditNoteRepo = new MockCreditNoteRepo();
+  usecase = new GetCreditNoteByInvoiceIdUsecase(mockCreditNoteRepo);
+});
+
+After({ tags: '@ValidateGetCreditNoteByInvoiceIdUsecase' }, () => {
+  mockCreditNoteRepo = null;
+  usecase = null;
+});
+
+const defaultContext: UsecaseAuthorizationContext = {
+  roles: [Roles.SUPER_ADMIN],
+};
+
+Given(
+  /^the credit note with an invoice id "([\w-]+)"/,
+  async (invoiceId: string) => {
+    creditNote = makeCreditNoteData(invoiceId);
+    creditNote = await mockCreditNoteRepo.save(creditNote);
+  }
+);
+
+When(
+  /^we call GetCreditNoteByInvoiceIdUsecase with invoice id "([\w-]+)"/,
+  async (invoiceId: string) => {
+    result = await usecase.execute({ invoiceId });
+  }
+);
+
+Then(/^the credit note is returned/, () => {
+  expect(result.value.isSuccess).to.equal(true);
+});
