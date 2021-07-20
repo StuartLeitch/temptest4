dbaf65bd2090247a47cd31e91c0a7aee22fa76be
Lucian Lature <lucian.lature@hindawi.com>
2021-07-20 11:39:28 +0300

    UTC format for Coupons added.

--- apps/invoicing-admin/src/routes/Coupon/components/CouponViewEdit.tsx
+++ apps/invoicing-admin/src/routes/Coupon/components/CouponViewEdit.tsx
@@ -1,7 +1,8 @@
 import React from 'react';
 
 import { Form } from '../../../components';
-import { TypeSelection, Code, Reduction, Name, Date, Redeems } from './Fields';
+import  { formatDate } from '../../../utils/date';
+import { TypeSelection, Code, Reduction, Name, DateField, Redeems } from './Fields';
 
 import { CouponType, CouponMode } from '../types';
 
@@ -66,24 +67,24 @@ const CouponViewEdit: React.FC<CouponViewEditProps> = ({ coupon, mode }) => {
 
       <Redeems value={redeemCount} />
 
-      <Date
-        stringValue={dateCreated}
+      <DateField
+        stringValue={formatDate(Date.parse(dateCreated))}
         disabled
         label='Created Date'
         id='createdDate'
         mode={mode}
       />
 
-      <Date
-        stringValue={dateUpdated}
+      <DateField
+        stringValue={formatDate(Date.parse(dateUpdated))}
         disabled
         label='Updated Date'
         id='updatedDate'
         mode={mode}
       />
 
-      <Date
-        stringValue={expirationDate}
+      <DateField
+        stringValue={formatDate(Date.parse(expirationDate))}
         disabled={areInputsDisabled}
         label='Expiration Date'
         id='expirationDate'
