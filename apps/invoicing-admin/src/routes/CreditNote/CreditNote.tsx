b5efa926ed41a3bea0688f09562cc503f3b0acb6
Silviu Popovici <silviu.popovici94@gmail.com>
2021-08-09 11:06:52 +0300

    refactor: removed CN timeline

--- apps/invoicing-admin/src/routes/CreditNote/CreditNote.tsx
+++ apps/invoicing-admin/src/routes/CreditNote/CreditNote.tsx
@@ -23,7 +23,6 @@ import {
 } from '../../components';
 
 import { HeaderMain } from '../components/HeaderMain'
-import { TimelineMini } from '../components/Timeline/TimelineMini';
 
 import { CREDIT_NOTE_QUERY } from './graphql';
 import CreditNoteDetailsTab from './components/CreditNoteDetailsTab';
@@ -150,79 +149,6 @@ const Details: React.FC = () => {
                 </TabPane>
               </UncontrolledTabs.TabContent>
             </UncontrolledTabs>
-          </Col>
-          <Col lg={4}>
-            {/* START Card Widget */}
-            <Card className='mb-3'>
-              <CardBody>
-                <CardTitle tag='h6'>Timeline</CardTitle>
-                {invoice?.dateCreated && (
-                  <TimelineMini
-                    icon='circle'
-                    iconClassName='text-success'
-                    badgeTitle='Final'
-                    badgeColor='success'
-                    date={format(
-                      new Date(invoice?.dateCreated),
-                      'dd MMMM yyyy'
-                    )}
-                    phrase={'Credit Note enters FINAL state.'}
-                  />
-                )}
-                {/* {invoice?.dateIssued && (
-                  <TimelineMini
-                    icon='times-circle'
-                    iconClassName='text-primary'
-                    badgeTitle='Active'
-                    badgeColor='primary'
-                    date={format(new Date(invoice?.dateIssued), 'dd MMMM yyyy')}
-                    phrase={'Credit Note enters ACTIVE state.'}
-                  />
-                )} */}
-                {/* {invoice?.payments?.length > 0 && (
-                  <TimelineMini
-                    icon='check-circle'
-                    iconClassName='text-success'
-                    badgeTitle='Paid'
-                    badgeColor='success'
-                    date={format(
-                      invoice?.payments
-                        ?.map(i => new Date(i.dateCreated))
-                        .sort(compareDesc)[0],
-                      'dd MMMM yyyy'
-                    )}
-                    phrase={'Credit Note enters FINAL state.'}
-                  />
-                )} */}
-                {invoice?.creditNote && (
-                  <TimelineMini
-                    icon='check-circle'
-                    iconClassName='text-warning'
-                    badgeTitle='Credit Note'
-                    badgeColor='warning'
-                    date={format(
-                      new Date(invoice?.creditNote?.dateCreated),
-                      'dd MMMM yyyy'
-                    )}
-                    phrase={'Credit Note issued.'}
-                  />
-                )}
-                {invoice?.invoiceItem?.article?.datePublished && (
-                  <TimelineMini
-                    icon='play-circle'
-                    iconClassName='text-blue'
-                    badgeTitle='Published'
-                    badgeColor='blue'
-                    date={format(
-                      new Date(invoice?.invoiceItem?.article?.datePublished),
-                      'dd MMMM yyyy'
-                    )}
-                    phrase={'Article enters PUBLISHED state.'}
-                  />
-                )}
-              </CardBody>
-            </Card>
-            {/* END Card Widget */}
           </Col>
         </Row>
       </Container>
