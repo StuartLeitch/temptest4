b8af4dc7cbca9cf2c73376f12f8dd83ba0cd471d
Lucian Lature <lucian.lature@hindawi.com>
2021-08-13 09:46:23 +0300

    Fix deploy by adding migration file.

---
+++ libs/shared/src/lib/infrastructure/database/knex/migrations/20210812143533_add_audit_log_table.js
@@ -0,0 +1,16 @@
+module.exports.up = async function(knex) {
+  return knex.schema
+    .createTable('audit_logs', function(table) {
+      table.string('user_account', 40).primary();
+      table.string('entity', 40);
+      table.string('action', 40);
+      table.datetime('timestamp', { precision: 2, useTz: false });
+      table.string('old_value');
+      table.string('current_value');
+    });
+};
+
+module.exports.down = async function(knex) {
+  //await knex.raw('DROP EXTENSION IF EXISTS "uuid"')
+  knex.schema.dropTable('audit_logs');
+};
