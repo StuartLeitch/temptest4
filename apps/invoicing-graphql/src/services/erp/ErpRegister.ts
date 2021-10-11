9228fb09319d32d0be264ad06fccc20c1ed192ec
Lucian Lature <lucian.lature@hindawi.com>
2021-10-11 11:33:29 +0300

    Do not register the same invoice twice.

--- apps/invoicing-graphql/src/services/erp/ErpRegister.ts
+++ apps/invoicing-graphql/src/services/erp/ErpRegister.ts
@@ -1,22 +1,6 @@
 const { Producer } = require('sqs-producer');
 import { v4 as uuidv4 } from 'uuid';
 
-// // create custom producer (supporting all opts as per the API docs: http://docs.aws.amazon.com/AWSJavaScriptSDK/latest/AWS/SQS.html#constructor-property)
-// const producer = Producer.create({
-//   region: 'eu-west-1',
-//   queueUrl: 'https://sqs.eu-west-1.amazonaws.com/916437579680/invoice-registration',
-//   accessKeyId: 'AKIA5KX7PW6QKZ5FZWOJ',
-//   secretAccessKey: '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'
-// });
-
-// // send messages to the queue
-// (async() => {
-//   await producer.send({
-//     'id': 'id1',
-//     'body': JSON.stringify({'foo': 'bar'})
-//   });
-// })();
-
 import { env } from '../../env';
 
 export class ErpRegister {
