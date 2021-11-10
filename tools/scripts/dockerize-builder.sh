6ae0531bb4f7e5f3c6f56d303ead66f2202ba4e5
Lucian Lature <lucian.lature@hindawi.com>
2021-11-10 13:48:44 +0200

    Do not use cache from.

--- tools/scripts/dockerize-builder.sh
+++ tools/scripts/dockerize-builder.sh
@@ -1,6 +1,7 @@
 set -e
 
 docker pull $LATEST_BUILD_IMAGE || true
-docker build --cache-from $LATEST_BUILD_IMAGE -t $BUILD_IMAGE -t $LATEST_BUILD_IMAGE -f Dockerfile.build .
+# docker build --cache-from $LATEST_BUILD_IMAGE -t $BUILD_IMAGE -t $LATEST_BUILD_IMAGE -f Dockerfile.build .
+docker build -t $BUILD_IMAGE -t $LATEST_BUILD_IMAGE -f Dockerfile.build .
 docker push $BUILD_IMAGE
 docker push $LATEST_BUILD_IMAGE
