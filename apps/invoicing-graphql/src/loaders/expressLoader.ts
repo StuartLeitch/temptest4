3e6f60399370c047ab0a1a8fc6dab0eba9039117
Lucian Lature <lucian.lature@hindawi.com>
2021-12-09 16:10:53 +0200

    fix(invoicing-admin): :bug: Refs PPBK-1815 | Fix the date selector bug when downloading.

--- apps/invoicing-graphql/src/loaders/expressLoader.ts
+++ apps/invoicing-graphql/src/loaders/expressLoader.ts
@@ -173,7 +173,7 @@ export const expressLoader: MicroframeworkLoader = (
           pagination: { offset: 0, limit: 10 },
           filters: {
             startDate: moment(String(req.query.startDate)).format('YYYY-MM-D') ?? null,
-            endDate: moment(String(req.query.endDate)).add(1, 'days').format('YYYY-MM-D') ?? null,
+            endDate: moment(String(req.query.endDate)).format('YYYY-MM-D') ?? null,
             download: req.query.download ?? 1
           },
         },
