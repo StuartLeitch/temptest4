99eeb293420da0f1f76c1283ab2f46b631b6f80b
Razvan Ariciuc <razvan.ariciuc@hindawi.com>
2021-12-22 16:43:32 +0200

    Remove default admin cliend_id var

--- apps/invoicing-admin/chart/hindawi/automation/index.ts
+++ apps/invoicing-admin/chart/hindawi/automation/index.ts
@@ -1,7 +1,4 @@
-import {
-  WithAwsSecretsServiceProps,
-  IngressOptionsSpec,
-} from '@hindawi/phenom-charts';
+import { WithAwsSecretsServiceProps } from '@hindawi/phenom-charts';
 import { defaultValues } from '../../default';
 
 const values: WithAwsSecretsServiceProps = {
@@ -11,7 +8,6 @@ const values: WithAwsSecretsServiceProps = {
     ...defaultValues.serviceProps,
     envVars: {
       ...defaultValues.serviceProps.envVars,
-      AUTH_SERVER_CLIENT_ID: 'invoicing',
       AUTH_SERVER_REALM: 'Phenom',
       APP_NAME: 'Invoicing',
     },
