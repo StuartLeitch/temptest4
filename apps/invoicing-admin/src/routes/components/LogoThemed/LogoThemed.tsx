acdd55ef113f99704ebfbbbd19a2f27754a657e9
Silviu Popovici <silviu.popovici94@gmail.com>
2022-01-11 13:22:55 +0200

    fix: PPBK-424 logo changing fix
    using hindawi.svg only now

--- apps/invoicing-admin/src/routes/components/LogoThemed/LogoThemed.tsx
+++ apps/invoicing-admin/src/routes/components/LogoThemed/LogoThemed.tsx
@@ -5,16 +5,16 @@ import classNames from 'classnames';
 import { ThemeConsumer } from '../../../components/Theme';
 
 const logos = {
-  white: require('./../../../assets/images/logos/logo-white.svg'),
+  white: require('./../../../assets/images/logos/hindawi.svg'),
   primary: require('./../../../assets/images/logos/hindawi.svg'),
-  success: require('./../../../assets/images/logos/logo-success.svg'),
-  warning: require('./../../../assets/images/logos/logo-warning.svg'),
-  danger: require('./../../../assets/images/logos/logo-danger.svg'),
-  info: require('./../../../assets/images/logos/logo-info.svg'),
-  indigo: require('./../../../assets/images/logos/logo-indigo.svg'),
-  purple: require('./../../../assets/images/logos/logo-purple.svg'),
-  pink: require('./../../../assets/images/logos/logo-pink.svg'),
-  yellow: require('./../../../assets/images/logos/logo-yellow.svg')
+  success: require('./../../../assets/images/logos/hindawi.svg'),
+  warning: require('./../../../assets/images/logos/hindawi.svg'),
+  danger: require('./../../../assets/images/logos/hindawi.svg'),
+  info: require('./../../../assets/images/logos/hindawi.svg'),
+  indigo: require('./../../../assets/images/logos/hindawi.svg'),
+  purple: require('./../../../assets/images/logos/hindawi.svg'),
+  pink: require('./../../../assets/images/logos/hindawi.svg'),
+  yellow: require('./../../../assets/images/logos/hindawi.svg'),
 };
 
 const getLogoUrl = (style, color) => {
@@ -30,9 +30,13 @@ const getLogoUrlBackground = (style, color) => {
   }
 };
 
-const LogoThemed: React.FC<LogoThemedProps> = ({ checkBackground, className, ...otherProps }) => (
+const LogoThemed: React.FC<LogoThemedProps> = ({
+  checkBackground,
+  className,
+  ...otherProps
+}) => (
   <ThemeConsumer>
-    {({ style, color }: {style: string; color: string}) => (
+    {({ style, color }: { style: string; color: string }) => (
       <img
         src={
           checkBackground
@@ -48,12 +52,12 @@ const LogoThemed: React.FC<LogoThemedProps> = ({ checkBackground, className, ...
 );
 LogoThemed.propTypes = {
   checkBackground: PropTypes.bool,
-  className: PropTypes.string
+  className: PropTypes.string,
 };
 
 interface LogoThemedProps {
   checkBackground?: boolean;
   className?: string;
-};
+}
 
 export { LogoThemed };
