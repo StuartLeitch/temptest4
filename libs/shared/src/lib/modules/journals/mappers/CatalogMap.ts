7514d9991a6d840df3ef4dca416feac3c218fc83
Lucian Lature <lucian.lature@hindawi.com>
2022-02-02 13:44:32 +0200

    fix(invoicing-graphql): :ambulance: Fix the mappings in CatalogMap.

--- libs/shared/src/lib/modules/journals/mappers/CatalogMap.ts
+++ libs/shared/src/lib/modules/journals/mappers/CatalogMap.ts
@@ -23,7 +23,7 @@ export class CatalogMap extends Mapper<CatalogItem> {
           ? PublisherId.create(new UniqueEntityID(raw.publisherId))
           : null,
         issn: raw.issn,
-        code: raw.code,
+        code: raw.journalCode,
       },
       new UniqueEntityID(raw.id)
     );
@@ -42,7 +42,7 @@ export class CatalogMap extends Mapper<CatalogItem> {
       journalTitle: catalogItem.journalTitle,
       publisherId: catalogItem.publisherId.id.toString(),
       issn: catalogItem.issn,
-      code: catalogItem.code,
+      journalCode: catalogItem.code,
     };
   }
 }
