3d1026b3e3c6ed82a7464ec0f98c9689f36b65a8
silviu.popovici <silviu.popovici94@gmail.com>
2022-02-09 10:21:41 +0200

    refactor: PPBK-2712 | fixed journalCode naming

--- libs/shared/src/lib/modules/journals/domain/CatalogItem.ts
+++ libs/shared/src/lib/modules/journals/domain/CatalogItem.ts
@@ -14,7 +14,7 @@ export interface CatalogItemProps {
   journalId: JournalId;
   journalTitle?: string;
   issn?: string;
-  code?: string;
+  journalCode?: string;
   created?: Date;
   updated?: Date;
   publisherId?: PublisherId;
@@ -67,8 +67,8 @@ export class CatalogItem extends AggregateRoot<CatalogItemProps> {
     return this.props.issn;
   }
 
-  public get code(): string {
-    return this.props.code;
+  public get journalCode(): string {
+    return this.props.journalCode;
   }
 
   public get created(): Date {
