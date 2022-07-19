0d7cdba00b36c4afa29345424bffe8d03151e6a9
Samuel Fathy <samuel.fathy@hindawi.com>
2022-07-19 16:42:40 +0200

    - Fixing log module error happening during application initialization due to logging of empty strings!

--- apps/reporting-backend/src/lib/banner.ts
+++ apps/reporting-backend/src/lib/banner.ts
@@ -3,13 +3,17 @@ import { LoggerContract } from 'libs/shared/src/lib/infrastructure/logging';
 
 export function banner(log: LoggerContract): void {
   log.info(`App is ready on ${env.app.port}`);
-  log.info(`To shut it down, press <CTRL> + C at any time.`);
-  log.info(``);
+  log.info(`To shut it down, press <CTRL> + C at any time.
+  `);
+  // log.info(``);
   log.info('-------------------------------------------------------');
   log.info(`Environment  : ${env.node}`);
-  log.info(`Version      : ${env.app.version}`);
-  log.info(``);
+  log.info(`Version      : ${env.app.version}
+  `);
+  // log.info(``);
 
-  log.info('-------------------------------------------------------');
-  log.info('');
+  log.info(`-------------------------------------------------------
+  `);
+  // log.info('-------------------------------------------------------');
+  // log.info('');
 }
