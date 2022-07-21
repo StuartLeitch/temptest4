6ebb1c1cfeccc7b4ea6db49f6b446421ad60ace2
Samuel Fathy <samuel.fathy@hindawi.com>
2022-07-21 16:13:59 +0200

    - Restore the correct version of deploy-ci.sh script on "feature/sb-refresh-and-migrations" branch

--- tools/scripts/deploy-ci.sh
+++ tools/scripts/deploy-ci.sh
@@ -2,10 +2,6 @@ set -e
 set -x
 
 rm -rf dist-k8s
-if [ $REQUIRED_APPS=="reporting-backend" -a $NAMESPACE=="prod-hindawi" ]; then
-    echo "Forcing install dependencies for reporting-backend production deployment to workaround cahce error!"
-    yarn install
-fi
 yarn build invoicing-infrastructure
 node dist/apps/invoicing-infrastructure/main.js
-# kubectl apply -f ./dist-k8s/ --namespace=$NAMESPACE
+kubectl apply -f ./dist-k8s/ --namespace=$NAMESPACE
