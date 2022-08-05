950adfabae09a44c3ad68db5b96fc1c50ced4f2a
Rares Stan <rstan@wiley.com>
2022-08-05 11:51:51 +0300

    fix: PPBK-4465 | Enable the credit note and draft invoice events regen

--- apps/invoicing-graphql/src/graphql/resolvers/index.ts
+++ apps/invoicing-graphql/src/graphql/resolvers/index.ts
@@ -3,20 +3,18 @@ import { merge } from 'lodash';
 import { Context } from '../../builders';
 import { Resolvers } from '../schema';
 
-// import { generateInvoiceDraftCompensatoryEvents } from './generateInvoiceDraftCompensatoryEvents';
-// import { generateCreditNoteCompensatoryEvents } from './generateCreditNoteCompensatoryEvents';
+import { generateInvoiceDraftCompensatoryEvents } from './generateInvoiceDraftCompensatoryEvents';
+import { generateCreditNoteCompensatoryEvents } from './generateCreditNoteCompensatoryEvents';
 import { generateInvoiceCompensatoryEvents } from './generateInvoiceCompensatoryEvents';
-import { creditNote } from './creditNote';
-import { reminders } from './reminders';
 import { invoicingJournals } from './journals';
+import { creditNote } from './creditNote';
+import { publisher } from './publishers';
+import { reminders } from './reminders';
 import { payments } from './payments';
 import { invoice } from './invoice';
 import { coupon } from './coupon';
-// import { generateCompensatoryEvents } from './generateCompensatoryEvents';
-// import { generateDraftCompensatoryEvents } from './generateDraftCompensatoryEvents';
 import { audit } from './audit';
 import { payer } from './payer';
-import { publisher } from './publishers';
 
 export const resolvers: Resolvers<Context> = merge(
   {},
@@ -27,10 +25,9 @@ export const resolvers: Resolvers<Context> = merge(
   payments,
   invoicingJournals,
   publisher,
-  generateInvoiceCompensatoryEvents,
   reminders,
-  // generateDraftCompensatoryEvents,
-  audit
-  // generateInvoiceDraftCompensatoryEvents,
-  // generateCreditNoteCompensatoryEvents
+  audit,
+  generateInvoiceDraftCompensatoryEvents,
+  generateCreditNoteCompensatoryEvents,
+  generateInvoiceCompensatoryEvents
 );
