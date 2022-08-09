20fc2527c32787dbae88da1d3ecb7b8b4dadf5ed
Samuel Fathy <samuel.fathy@hindawi.com>
2022-08-09 18:33:26 +0200

    - Temporarily disable applying k8s deployment and allow running the prod deployment from the reporting-backend to test the generated prod deployment file

--- tools/scripts/deploy-ci.sh
+++ tools/scripts/deploy-ci.sh
@@ -2,6 +2,17 @@ set -e
 set -x
 
 rm -rf dist-k8s
-yarn build invoicing-infrastructure
+#generate kube manifests
 node dist/apps/invoicing-infrastructure/main.js
-kubectl apply -f ./dist-k8s/ --namespace=$NAMESPACE
+#delete potential orphans, changing the phenom-charts version messes with resource name generation and this leads to duplicate resources creation,
+#where new deployments are created instead of detecting the old ones
+#kubectl -n $NAMESPACE get deployments | awk '/invoicing|import/{print $1}' | xargs kubectl delete -n $NAMESPACE deployment || true
+#rollout the changes
+# kubectl apply -f ./dist-k8s/ --namespace=$NAMESPACE
+# echo 'Waiting for pod startup...'
+# #await container status
+# kubectl -n $NAMESPACE get deployments | awk '/invoicing|import/{print $1}' | while read line; do
+#   kubectl rollout status deployments -n $NAMESPACE  $line
+# done
+# echo 'Waiting for pod startup... Done!'
+
