87031af5e10edf264ddf9ad53fc6ca53dc1c2404
Samuel Fathy <samuel.fathy@hindawi.com>
2022-08-10 13:11:23 +0200

    Revert "- Temporarily disable applying k8s deployment and allow running the prod deployment from the reporting-backend to test the generated prod deployment file"
    This reverts commit ef4161340eff2204adb8eb899bd569b5a1d880ff.

--- tools/scripts/deploy-ci.sh
+++ tools/scripts/deploy-ci.sh
@@ -8,11 +8,11 @@ node dist/apps/invoicing-infrastructure/main.js
 #where new deployments are created instead of detecting the old ones
 #kubectl -n $NAMESPACE get deployments | awk '/invoicing|import/{print $1}' | xargs kubectl delete -n $NAMESPACE deployment || true
 #rollout the changes
-# kubectl apply -f ./dist-k8s/ --namespace=$NAMESPACE
-# echo 'Waiting for pod startup...'
-# #await container status
-# kubectl -n $NAMESPACE get deployments | awk '/invoicing|import/{print $1}' | while read line; do
-#   kubectl rollout status deployments -n $NAMESPACE  $line
-# done
-# echo 'Waiting for pod startup... Done!'
+kubectl apply -f ./dist-k8s/ --namespace=$NAMESPACE
+echo 'Waiting for pod startup...'
+#await container status
+kubectl -n $NAMESPACE get deployments | awk '/invoicing|import/{print $1}' | while read line; do
+  kubectl rollout status deployments -n $NAMESPACE  $line
+done
+echo 'Waiting for pod startup... Done!'
 
